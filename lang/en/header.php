0a147100df802af4a99b84428b39b63327b3900c
MuchQuak <loganpwilt@gmail.com>
2023-08-24 12:44:59 -0700

    Adding some language ports

---
+++ lang/en/header.php
@@ -0,0 +1,51 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | English Translations for Header View
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'home' =>'Home',
+    'search' =>'Search',
+    'collections' =>'Collections',
+    'map' =>'Map',
+    'map_search' =>'Map Search',
+    'taxonomic_explorer' =>'Taxonomic Explorer',
+    'images' =>'Images',
+    'image_browser' =>'Image Browser',
+    'image_search' =>'Image Search',
+    'species_checklists' =>'Species Checklists',
+    'checklist_project 1' =>'Checklist Project 1',
+    'checklist_project 2' => 'Checklist Project 2',
+    'dynamic_species_list' =>'Dynamic Species List',
+    'identification_keys' =>'Identification Keys',
+    'amphibians' =>'Amphibians',
+    'birds' =>'Birds',
+    'fish' =>'Fish',
+    'invertebrates' =>'Invertebrates',
+    'mammals' =>'Mammals',
+    'plants'=>'Plants',
+    'reptiles'=>'Reptiles',
+    'more_information'=>'More Information',
+    'about_project'=>'About Project',
+    'partners'=>'Partners',
+    'contacts'=>'Contacts',
+    'symbiota_help'=>'Symbiota Help',
+    'sitemap'=>'Sitemap',
+    'data_use'=>'Data Use',
+    'welcome'=>'Welcome',
+    'my_profile'=>'My Profile',
+    'sign_out'=>'Sign Out',
+    'sign_in'=>'Sign In',
+    'new_account'=>'New Account',
+];
+
+?>
+
