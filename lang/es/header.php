16dacb5c4a6c3b053c7e415f70b616fd3b4a32cc
MuchQuak <loganpwilt@gmail.com>
2023-08-24 12:44:59 -0700

    Adding some language ports

---
+++ lang/es/header.php
@@ -0,0 +1,53 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | English Translations for Header View
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+
+    'home' =>'Inicio',
+    'search' =>'Búsqueda',
+    'collections' =>'Colecciones',
+    'map' =>'Mapa',
+    'map_search' =>'Búsqueda Mapa',
+    'taxonomic_explorer' =>'Explorador Taxon&oacute;mico',
+    'images' =>'Im&aacute;genes',
+    'image_browser' =>'Navegador de Im&aacute;genes',
+    'image_search' =>'Búsqueda de Im&aacute;genes',
+    'species_checklists' =>'Lista de Especies',
+    'checklist_project 1' =>'Proyecto 1',
+    'checklist_project 2' => 'Proyecto 2',
+    'dynamic_species_list' =>'Listados Din&aacute;micos',
+    'identification_keys' =>'Claves de identificación',
+
+    'amphibians' =>'Anfibios',
+    'birds' =>'Aves',
+    'fish' =>'Peces',
+    'invertebrates' =>'Invertebrados',
+    'mammals' =>'Mamíferos',
+    'plants'=>'Plantas',
+    'reptiles'=>'Reptiles',
+
+    'more_information'=>'M&aacute;s Informaci&oacute;n',
+    'about_project'=>'Información sobre el Proyecto',
+    'partners'=>'Socios',
+    'contacts'=>'Contactos',
+    'symbiota_help'=>'Ayuda de Symbiota',
+    'sitemap'=>'Mapa del Sitio',
+    'data_use'=>'Uso de Datos',
+
+    'welcome'=>'Bienvenido',
+    'my_profile'=>'Mi Perfil',
+    'sign_out'=>'Finalizar Sesi&oacute;n',
+    'sign_in'=>'Iniciar Sesi&oacute;n',
+    'new_account'=>'Nueva Cuenta',
+];
+
+?>
