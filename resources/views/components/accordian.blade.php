eff55403303461440adf680a5314ada0adfb5949
MuchQuak <loganpwilt@gmail.com>
2024-01-20 00:30:18 -0800

    Moving Search router to point to new page

--- resources/views/components/accordian.blade.php
+++ resources/views/components/accordian.blade.php
@@ -1,7 +1,7 @@
-@props(['id', 'label', 'name', 'checked' => false])
+@props(['id', 'label', 'name', 'open' => false])
 <section>
 	<!-- Accordion selector -->
-    <input id="{{ $id }}" type="checkbox" name="{{ $name?? $label }}" class="accordion-selector" @checked($checked) />
+    <input id="{{ $id }}" type="checkbox" name="{{ $name?? $label }}" class="accordion-selector" @checked(old($id, $open)) />
     <!-- Accordion header -->
     <label for="{{ $id }}" class="accordion-header">{{ $label }}</label>
 
