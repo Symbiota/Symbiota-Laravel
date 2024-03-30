de97cfd9de11e694652db1ffb6e444222c0b2fbd
MuchQuak <loganpwilt@gmail.com>
2024-03-29 19:47:27 -0700

    updated home and footer

--- resources/views/core/layout.blade.php
+++ resources/views/core/layout.blade.php
@@ -9,22 +9,48 @@ $navigations = [
     ["title" => __("header.symbiota_help"), "link" => 'https =>//biokic.github.io/symbiota-docs/'],
     ["title" => __("header.sitemap"), "link" => config('portal.name') . 'sitemap.php'],
 ];
-//env('VITE_CSS_TARGET', 'resources/css/app.css')
+
+$logos = [
+    [
+        "img" => '/'. config('portal.name') . '/images/layout/logo_nsf.gif',
+        "link" => 'https://www.nsf.gov',
+        "title" => 'NSF'
+    ],
+    [
+        "img" => '/'. config('portal.name') . '/images/layout/logo_idig.png',
+        "link" => 'http://idigbio.org',
+        "title" => 'iDigBio'
+    ],
+    [
+        "img" => '/'. config('portal.name') . '/images/layout/logo-asu-biokic.png',
+        "link" => 'https://biokic.asu.edu',
+        "title" => 'Biodiversity Knowledge Integration Center'
+    ],
+];
+
+$grants= [
+    [
+        "link" => 'https://www.nsf.gov',
+        "label" => '#-------',
+        "grant_id" => ''
+    ],
+];
 @endphp
+
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite(['resources/js/app.js','resources/css/app.css'])
     @stack('css-styles')
-    @stack('js-libs')
-  </head>
+    @stack('js-libs') </head>
   <body>
-    <x-header>
-        <x-navbar :navigations="$navigations"/>
-    </x-header>
+    <x-header/>
+    <x-navbar :navigations="$navigations"/>
     {{ $slot }}
-    <x-footer/>
+    <div class="bg-[#F2F0E9] p-8">
+        <x-footer :logos="$logos" :grants="$grants"/>
+    </div>
     @stack('js-scripts')
   </body>
 </html>
