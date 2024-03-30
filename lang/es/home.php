a47d542fb8830a1059cf1af9af5c8b93cebf6a82
MuchQuak <loganpwilt@gmail.com>
2024-03-29 19:46:24 -0700

    added translations for home page

---
+++ lang/es/home.php
@@ -0,0 +1,18 @@
+<?php
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | French Translations for Home View
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used by the paginator library to build
+    | the simple pagination links. You are free to change them to anything
+    | you want to customize your views to better match your application.
+    |
+    */
+    'welcome-title' => 'Bienvenue',
+    'welcome-text' => 'Ce portail de données a été créé pour promouvoir la collaboration... Remplacer par le texte d\'introduction en anglais'
+];
+
+?>
