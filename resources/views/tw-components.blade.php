fdfd474f736b4ac5e547482c93bbf48e7b90f0d5
MuchQuak <loganpwilt@gmail.com>
2024-04-20 00:49:37 -0700

    Progress on tw component page

--- resources/views/tw-components.blade.php
+++ resources/views/tw-components.blade.php
@@ -1,26 +1,59 @@
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-
-<head>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    @vite(['resources/js/app.js','resources/css/app.css'])
-    @stack('css-styles')
-    @stack('js-libs')
-</head>
-
-<body class="min-h-screen flex flex-col w-[90%] max-w-screen-lg gap-y-4 mx-auto">
+<x-layout>
+    <div class="flex flex-col w-[90%] max-w-screen-lg gap-y-4 mx-auto">
         <h1 class="text-3xl my-3 font-bold font-sans text-primary">
             Tailwind Components
         </h1>
-        <x-swatch/>
-        <x-button>Button</x-button>
-        <button class="btn bg-transparent text-base-content border-secondary border w-fit" type="">Button</button>
-        <x-button class="bg-secondary">Button</x-button>
-        <x-input required :id="'input'" :label="'Input'" />
-        <x-checkbox :id="'checkbox'" :label="'checkbox'" />
-        <div class="bg-base-200">
-        <x-brand/>
+        <div class="flex gap-x-4 items-center">
+            <div class="bg-primary rounded-md w-32">
+                <x-brand />
+            </div>
+            <x-swatch />
         </div>
-</body>
+        <div class="flex gap-x-4">
+            <button class="btn">
+                Button 1
+            </button>
+            <button class="btn bg-secondary hover:bg-primary text-secondary-content hover:text-primary-content">
+                Button 2
+            </button>
+            <button class="btn bg-transparent text-base-content border-secondary border ">
+                Button 3
+            </button>
 
-</html>
+        </div>
+        <x-input required :id="'input'" :label="'Text Input'" />
+        <x-input required type="number" :id="'input'" :label="'Number Input'" />
+
+        <h1 class="text-xl my-2 font-bold font-sans text-error">
+           Todo Radio
+        </h1>
+        <h1 class="text-xl my-2 font-bold font-sans text-error">
+           Todo Checkbox
+        </h1>
+        <h1 class="text-xl my-2 font-bold font-sans text-error">
+           Todo Nested Checkbox
+        </h1>
+        <x-checkbox :id="'checkbox'" :label="'Checkbox'" />
+        <x-accordion :id="'Taxonomy'" :label="'Accordian'">
+            <x-input required :id="'input'" :label="'Text Input'" />
+            <x-input required type="number" :id="'input'" :label="'Number Input'" />
+        </x-accordion>
+
+        <x-accordion :id="'Nested'" :label="'Nested Accordian'">
+            <x-accordion :id="'Collections'" :label="'Accordian'">
+            <x-input required :id="'input'" :label="'Text Input'" />
+            <x-input required type="number" :id="'input'" :label="'Number Input'" />
+            </x-accordion>
+        </x-accordion>
+
+        <h1 class="text-xl my-2 font-bold font-sans text-error">
+           Todo Autocomplete Input
+        </h1>
+        <h1 class="text-xl my-2 font-bold font-sans text-error">
+           Todo Collection Result Card
+        </h1>
+        <h1 class="text-xl my-2 font-bold font-sans text-error">
+           Todo Collection Result Card
+        </h1>
+    </div>
+</x-layout>
