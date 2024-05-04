256e91e644ba1fc89e63f10584e1af458e63c56e
MuchQuak <loganpwilt@gmail.com>
2024-05-03 17:17:30 -0700

    adding htmx

--- resources/views/core/layout.blade.php
+++ resources/views/core/layout.blade.php
@@ -42,7 +42,6 @@ $grants= [
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-    <script src="https://unpkg.com/htmx.org@1.9.12"></script>
     @vite(['resources/js/app.js','resources/css/app.css'])
     @stack('css-styles')
     @stack('js-libs')
