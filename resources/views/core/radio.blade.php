1d4b3fc1af4a6611da2d4d3ec7fa25e7d5a16714
MuchQuak <loganpwilt@gmail.com>
2024-09-13 19:11:13 -0700

    better radio focus

--- resources/views/core/radio.blade.php
+++ resources/views/core/radio.blade.php
@@ -10,7 +10,7 @@
         <div class="grid place-items-center">
             <input
                 class="peer col-start-1 row-start-1 appearance-none shrink-0
-                w-6 h-6 border-2 border-accent disabled:border-neutral rounded-full"
+                w-6 h-6 border-2 hover:border-4 border-accent disabled:border-neutral rounded-full outline-none"
                 type="radio"
                 autocomplete="off"
                 name="{{ $name }}"
@@ -23,6 +23,15 @@
                 pointer-events-none
                 col-start-1 row-start-1 w-4 h-4 rounded-full
                 peer-checked:bg-accent
+            "
+            ></div>
+            <div class="
+                pointer-events-none
+                col-start-1 row-start-1 w-7 h-7 rounded-full
+                opacity-70
+                ring-accent
+                peer-hover:ring-4
+                peer-focus:ring-4
             "
             ></div>
         </div>
