db446dd1d36f1f794da1c862171739ee1f9bc06f
MuchQuak <loganpwilt@gmail.com>
2024-09-15 17:19:52 -0700

    cleaned up web routes file via Route::view and seperating controller logic

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,15 +6,14 @@ use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\View\Compilers\BladeCompiler;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
+
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
-        //
+    public function register(): void {
     }
+
 
     /**
      * Bootstrap any application services.
