bce1a096e2db8bf8eca12b814a937088be780cc6
MuchQuak <loganpwilt@gmail.com>
2024-09-18 14:53:33 -0700

    add error handling to login and boosted redirects so page transitions are snappy

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\MessageBag;
 
 class LoginController extends Controller {
 
@@ -14,38 +16,54 @@ class LoginController extends Controller {
     }
 
     public static function login(Request $request) {
-        $credentials = $request->validate([
-            'email' => ['required', 'email'],
-            'password' => ['required'],
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email',
+            'password' => 'required',
         ]);
+
+        if($validator->fails()) {
+            session()->flashInput($request->input());
+            return view('pages/login', [
+                'errors' => $validator->errors()
+            ])->fragment('form');
+        }
+
+        $validated = $validator->validated();
 
         $result = DB::select('
             SELECT uid
             FROM users
             WHERE (old_password = CONCAT(\'*\', UPPER(SHA1(UNHEX(SHA1(?)))))) AND
             email = ?',
-            [$credentials['password'], $credentials['email']]
+            [$validated['password'], $validated['email']]
         );
+
 
         //Check old password
         if(count($result) > 0) {
             //Update Hashing to use new has then clear the old password for security
             DB::table('users')
                 ->update([
-                    'password' => Hash::make($credentials['password']),
+                    'password' => Hash::make($validated['password']),
                     // TODO (Logan) offical verison should include this not doing currently so I don't mess up my login on my local db
                     //'old_password' => null,
                 ]);
         }
 
-        if (Auth::attempt($credentials)) {
+        if(Auth::attempt($validated)) {
             $request->session()->regenerate();
-            return redirect()->intended('/');
-        } else {
-            var_dump('failure');
+            return response(view('pages/home'))
+                ->header('HX-Replace-URL', '/')
+                ->header('HX-Retarget', 'body')
+                ->header('HX-Boosted', 'true');
         }
 
-        return view('pages/login');
+        session()->flashInput($request->input());
+        return response(
+            view('pages/login', [
+                'errors' => new MessageBag(['Invalid email or password'])
+            ])->fragment('form')
+        )->setStatusCode(401);
     }
 
     public static function logout(Request $request) {
