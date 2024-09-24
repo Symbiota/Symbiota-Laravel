a268fca793b758db8a33c02f65a78cbe0b436c29
MuchQuak <loganpwilt@gmail.com>
2024-09-24 11:03:09 -0700

    remove no longer used vite config for target

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,13 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-export default defineConfig(({mode}) =>{
-    const env = loadEnv(mode, process.cwd(), '');
+export default defineConfig(() => {
     return {
         plugins: [
             laravel({
                 input: [
                     'resources/js/app.js',
-                    env.VITE_CSS_TARGET?
-                        env.VITE_CSS_TARGET:
-                        'resources/css/app.css',
+                    'resources/css/app.css'
                 ],
                 refresh: true,
             }),
