c9f59a40aa2e1c980e672caa32d919a757e247dc
MuchQuak <loganpwilt@gmail.com>
2024-09-25 13:21:34 -0700

    add query logging for dev and clear log command

---
+++ app/Console/Commands/Log/ClearLogFile.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands\Log;
+
+use Illuminate\Console\Command;
+
+class ClearLogFile extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'log:clear';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Clear Laravel Log';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+        exec('echo "" > ' . storage_path('logs/laravel.log'));
+        exec('echo "" > ' . storage_path('logs/query.log'));
+        $this->info('Logs have been cleared');
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\View\Compilers\BladeCompiler;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\File;
 
 class AppServiceProvider extends ServiceProvider {
 
@@ -28,5 +30,18 @@ class AppServiceProvider extends ServiceProvider {
                 return \preg_replace('/\s*@trim\s*/m', '', $value);
             });
         });
+
+
+        /**
+         * Add Query logs For Local Enviroments.
+         */
+        if(config('app.env') === 'local') {
+            DB::listen(function($query) {
+                File::append(
+                    storage_path('/logs/query.log'),
+                    '[' . date('Y-m-d H:i:s') . ']' . PHP_EOL . $query->sql . ' [' . implode(', ', $query->bindings) . ']' . PHP_EOL . PHP_EOL
+                );
+            });
+        }
     }
 }
