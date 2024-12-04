2c182f138db2cda2dbf5913839d2149f5d63c7c5
MuchQuak <loganpwilt@gmail.com>
2024-12-03 17:29:12 -0800

    adjust provider to store occid value and have correct token url function header

--- app/Socialite/Orcid/Provider.php
+++ app/Socialite/Orcid/Provider.php
@@ -3,6 +3,8 @@
 namespace App\Socialite\Orcid;
 
 use GuzzleHttp\RequestOptions;
+use Illuminate\Support\Arr;
+use Laravel\Socialite\Two\InvalidStateException;
 use SocialiteProviders\Manager\OAuth2\AbstractProvider;
 use SocialiteProviders\Manager\OAuth2\User;
 
@@ -14,7 +16,8 @@ class Provider extends AbstractProvider {
     protected $scopeSeparator = ' ';
 
     protected $base_uri = 'https://orcid.org/';
-    protected $api_uri = 'https://orcid.org/';
+    protected $api_uri = 'https://api.orcid.org/';
+    protected $orcid;
 
     protected function getAuthUrl($state): string {
         return $this->buildAuthUrlFromBase($this->base_uri . 'oauth/authorize', $state);
@@ -28,8 +31,8 @@ class Provider extends AbstractProvider {
     /**
      * {@inheritdoc}
      */
-    protected function getUserByToken($token, $orcid) {
-        $response = $this->getHttpClient()->get($this->api_uri . '/v3.0/' . $orcid, [
+    protected function getUserByToken($token) {
+        $response = $this->getHttpClient()->get($this->api_uri . '/v3.0/' . $this->orcid . '/record', [
             RequestOptions::HEADERS => [
                 'Authorization' => 'Bearer '.$token,
             ],
@@ -43,15 +46,37 @@ class Provider extends AbstractProvider {
      */
     protected function mapUserToObject(array $user) {
         return (new User)->setRaw($user)->map([
-            'id'       => $user['id'],
-            'nickname' => $user['username'],
+            'uid'       => $user['id'],
+            //'nickname' => $user['username'],
             'name'     => $user['name'],
             'email'    => $user['email'],
-            'avatar'   => $user['avatar_url'],
+            //'avatar'   => $user['avatar_url'],
         ]);
     }
 
+    /**
+     * {@inheritdoc}
+     */
+    public function user()
+    {
+        if ($this->user) {
+            return $this->user;
+        }
+
+        if ($this->hasInvalidState()) {
+            throw new InvalidStateException;
+        }
+
+        $response = $this->getAccessTokenResponse($this->getCode());
+        $this->orcid = Arr::get($response, 'orcid');
+
+        $user = $this->getUserByToken(Arr::get($response, 'access_token'));
+
+        return $this->userInstance($response, $user);
+    }
+
+    /*
     public static function additionalConfigKeys(): array {
         return ['instance_uri'];
-    }
+    }*/
 }
