5b9c4d7d6e95c793198c491d34908dad8da060d4
MuchQuak <loganpwilt@gmail.com>
2025-02-26 17:02:11 -0800

    Add Collection Download file controller

--- routes/web.php
+++ routes/web.php
@@ -83,6 +83,7 @@ Route::group(['prefix' => '/occurrence'], function () {
 */
 Route::group(['prefix' => '/collections'], function () {
     Route::get('/search', [CollectionController::class, 'searchPage']);
+    Route::get('/download/file', [CollectionController::class, 'downloadFile']);
     Route::get('/download', [CollectionController::class, 'downloadPage']);
     Route::get('/table', [CollectionController::class, 'tablePage']);
     Route::get('/list', [CollectionController::class, 'listPage']);
