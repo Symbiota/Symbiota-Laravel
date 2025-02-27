e57e7b2e288129a4b256fb7312722f1e0535fe80
MuchQuak <loganpwilt@gmail.com>
2025-02-26 17:04:09 -0800

    Hook up htmx download request

--- resources/views/core/pages/collections/download.blade.php
+++ resources/views/core/pages/collections/download.blade.php
@@ -1,3 +1,23 @@
+@pushOnce('js-scripts')
+<script type="text/javascript">
+    /* Intercepts HTMX-get and forwards parameters to a download link. use htmx beforeRequest event */
+    function downloadHtmxRequest(event) {
+        event.preventDefault();
+
+        if(event.detail.requestConfig.verb !== 'get') {
+            console.error('downloadHtmxRequest only supports the "hx-get"')
+        }
+
+        const download_link = document.createElement('a');
+        document.body.appendChild(download_link)
+
+        const params = new URLSearchParams(event.detail.requestConfig.parameters)
+
+        download_link.href = event.detail.requestConfig.path + '?' + params.toString();
+        download_link.click();
+    }
+</script>
+@endPushOnce
 <x-layout class="flex flex-col gap-4">
     <h1 class="text-4xl font-bold">Data Usage Guidelines</h1>
     <p>
@@ -7,16 +27,23 @@
         supplied by the owning institution should take precedence over the general terms posted on the website.
     </p>
 
-    <form class="flex flex-col gap-4">
+    <form
+        hx-get="{{ url('collections/download/file') }}"
+        x-data="{}"
+        x-on:htmx:before-request="downloadHtmxRequest(event)"
+        hx-vals="{{ json_encode(request()->all()) }}"
+        class="flex flex-col gap-4"
+        >
         <x-radio label="Structure" name="schema" :default_value="'symbiota'"
             :options="[ ['label' => 'Symbiota Native', 'value' => 'symbiota'], ['label' => 'Darwin Core', 'value' => 'dwc']]" />
+        <input type="hidden" name="download" value="1">
 
         <fieldset class="flex flex-col gap-2">
             <legend class="text-xl">Data Extensions</legend>
-            <x-checkbox label="Include Determinations History" default_value="1" name="compressed" />
-            <x-checkbox label="Include Media Records" default_value="1" name="compressed" />
-            <x-checkbox label="Include Occurence Trait Attributes" default_value="1" name="compressed" />
-            <x-checkbox label="Include Alternative Identifiers" default_value="1" name="compressed" />
+            <x-checkbox label="Include Determinations History" default_value="1" name="include_determination_history" />
+            <x-checkbox label="Include Media Records" default_value="1" name="include_media" />
+            <x-checkbox label="Include Occurence Trait Attributes" default_value="1" name="include_occurrence_trait_attributes" />
+            <x-checkbox label="Include Alternative Identifiers" default_value="1" name="include_alternative_identifers" />
             * Output must be a compressed archive
         </fieldset>
 
