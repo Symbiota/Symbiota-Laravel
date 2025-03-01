d08b6f425bebbfbaae6b8787d0cfc75f9a630350
MuchQuak <loganpwilt@gmail.com>
2025-02-28 17:40:27 -0800

    add leaflet to window and add css

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,13 @@ import './bootstrap';
 import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus'
 import 'htmx.org';
+import L from 'leaflet';
 import { Chart, registerables } from 'chart.js';
 
 Chart.register(...registerables);
 window.Chart = Chart;
+
+window.L = L;
 
 window.Alpine = Alpine;
 Alpine.plugin(focus);
