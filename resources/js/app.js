d042f2e6a023d340de425fefadb2e96878badb94
MuchQuak <loganpwilt@gmail.com>
2025-03-10 11:00:43 -0700

    Import leaflet plugins via the js and css

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,12 +3,15 @@ import Alpine from 'alpinejs';
 import focus from '@alpinejs/focus'
 import 'htmx.org';
 import L from 'leaflet';
+import 'leaflet-draw';
+import 'leaflet.markercluster';
 import { Chart, registerables } from 'chart.js';
 
 Chart.register(...registerables);
 window.Chart = Chart;
 
 window.L = L;
+window.type = true;
 
 window.Alpine = Alpine;
 Alpine.plugin(focus);
