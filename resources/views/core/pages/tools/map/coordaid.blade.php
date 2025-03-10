f02eb13e5d408cc316dd362f19974786bbbaebe2
MuchQuak <loganpwilt@gmail.com>
2025-03-10 10:34:46 -0700

    setup basic scripts for coordaid

--- resources/views/core/pages/tools/map/coordaid.blade.php
+++ resources/views/core/pages/tools/map/coordaid.blade.php
@@ -1,3 +1,11 @@
+@pushOnce('js-scripts')
+<script type="text/javascript">
+    document.addEventListener('mapIntialized', function (e) {
+        let map = window.maps['map'];
+        L.marker([34, -112]).addTo(map);
+    });
+</script>
+@endPushOnce
 <x-layout class="p-0" :hasHeader="false" :hasNavbar="false" :hasFooter="false">
     <div class="p-4">
         <p>TODO (Logan) grab the help text</p>
@@ -8,5 +16,5 @@
             <x-button>Save</x-button>
         </div>
     </div>
-    <x-map />
+    <x-map id="map"/>
 </x-layout>
