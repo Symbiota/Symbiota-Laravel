a59e260bd6e1193097fd9333804469f987bff866
MuchQuak <loganpwilt@gmail.com>
2025-03-31 17:39:54 -0700

    Improve error spacing when multitple errors occur

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\Fortify\PasswordValidationRules;
+use App\Http\Responses\ProfileInformationUpdatedResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\MessageBag;
-use Laravel\Fortify\Fortify;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
 
 class UserProfileController extends Controller {
+    use PasswordValidationRules;
+
     public static function getProfile(Request $request) {
         $user = $request->user();
         $tokens = $user->tokens;
@@ -40,25 +46,45 @@ class UserProfileController extends Controller {
     }
 
     public static function updatePassword(Request $request) {
-        $new_password = request('new_password');
-        $confirm_password = request('confirm_password');
+        $user = $request->user();
+        $input = $request->input();
+/*
+        [
+            'current_password' => request('current_password'),
+            'password' => request('password'),
+        ];
+*/
 
         $error = null;
 
+        $validator = Validator::make($input, [
+            'current_password' => ['required', 'string', 'current_password:web'],
+            'password' => ['required', 'string', Password::default(), 'confirmed'],
+        ], [
+            'current_password.current_password' => __('The provided password does not match your current password.'),
+        ]);
+
+        if($validator->fails()) {
+            session()->flashInput($request->input());
+
+            return response(
+                view('pages/user/profile', ['errors' => $validator->errors()])
+                ->fragment('password')
+            );
+        }
+
+        $user->forceFill([
+            'password' => Hash::make($input['password']),
+        ])->save();
+
+        //return new ProfileInformationUpdatedResponse($request);
+
+        /*
         if(empty($new_password)) {
             $error = 'New Password cannot be empty';
         } else if($confirm_password != $new_password) {
             $error = 'New and Confirm Passwords must be equal';
-        }
-
-        if($error) {
-            session()->flashInput($request->input());
-
-            return response(
-                view('pages/user/profile', ['errors' => new MessageBag([$error])])
-                ->fragment('password')
-            );
-        }
+        }*/
 
         return response(
             view('pages/user/profile')
--- resources/views/core/errors.blade.php
+++ resources/views/core/errors.blade.php
@@ -1,5 +1,5 @@
 @if(count($errors) > 0)
-<div class="mb-4">
+<div class="mb-4 flex flex-col gap-4">
     @foreach ($errors->all() as $error)
     <div class="bg-error text-error-content rounded-md p-4">
         {{ $error }}
