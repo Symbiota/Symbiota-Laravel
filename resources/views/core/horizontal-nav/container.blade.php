5d6b7183d3e77a4cf261723ea79333a04cc2e1b6
MuchQuak <loganpwilt@gmail.com>
2025-04-09 13:46:59 -0700

    Add no wrap on horizontal nav contailer

--- resources/views/core/horizontal-nav/container.blade.php
+++ resources/views/core/horizontal-nav/container.blade.php
@@ -4,7 +4,7 @@
         @foreach ($items as $item)
         <button :class="active_tab === '{{ $item['label'] }}'? 'bg-base-200': 'bg-base-100' "
             @click="active_tab = '{{ $item['label'] }}'"
-            class="flex items-center gap-4 hover:bg-base-300 px-3 p-1 rounded-md relative cursor-pointer w-full">
+            class="flex items-center text-nowrap gap-4 hover:bg-base-300 px-3 p-1 rounded-md relative cursor-pointer w-full">
             <div x-show="active_tab === '{{ $item['label'] }}'" x-cloak
                 class="bg-accent w-1 h-5 absolute -left-2 rounded-md"></div>
             <span class="w-3 flex items-center justify-center">
