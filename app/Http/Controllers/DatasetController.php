7249607247bf4432b0e9ce6acd56448a4f02ebb9
MuchQuak <loganpwilt@gmail.com>
2025-04-11 17:03:42 -0700

    Rename dataset model

--- app/Http/Controllers/DatasetController.php
+++ app/Http/Controllers/DatasetController.php
@@ -2,14 +2,14 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\DatasetModel;
+use App\Models\Dataset;
 use Illuminate\Http\Request;
 
 class DatasetController extends Controller {
     public static function createDataset(Request $request) {
         $user = $request->user();
 
-        $new_dataset = new DatasetModel();
+        $new_dataset = new Dataset();
         $new_dataset->fill($request->all());
         $new_dataset->uid = $user->uid;
         $new_dataset->save();
--- app/Models/Dataset.php
+++ app/Models/Dataset.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class DatasetModel extends Model {
+class Dataset extends Model {
     use HasFactory;
 
     protected $table = 'omoccurdatasets';
