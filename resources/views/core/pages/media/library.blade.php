0ffa8e60376e3059e97ef92b8e94541f033651df
MuchQuak <loganpwilt@gmail.com>
2025-06-02 14:23:19 -0700

    Funtional Media Library

--- resources/views/core/pages/media/library.blade.php
+++ resources/views/core/pages/media/library.blade.php
@@ -17,19 +17,46 @@
         <x-button hx-boost="true" href="{{ url('media/search')}}">Media Search</x-button>
     </div>
 
-    <x-taxa-search />
+    <form hx-get="{{ url('media/library') }}" hx-target="#taxa_list" class="flex flex-col gap-4 border-y py-4">
+        <input type="hidden" name="fragment" value="taxa_list">
+        <x-taxa-search :hide_selector="true" :hide_synonyms_checkbox="true" />
+        <x-button type="submit">Search</x-button>
+    </form>
 
-    <hr>
+    <div class="flex items-center gap-2">
+        <x-button href="{{ url('media/library') }}?target=genus">Browse By Genus</x-button>
+        <x-button href="{{ url('media/library') }}?target=family">Browse By Family</x-button>
+    </div>
 
-    <h2 class="text-2xl font-bold">Select a family to see species list</h2>
-
-    <x-slide-tab-container :tabs="['Family', 'Genus']">
-        <x-slide-tab class="py-4">
-            TODO family Rendering
-        </x-slide-tab>
-
-        <x-slide-tab class="py-4">
-            TODO Genus Rendering
-        </x-slide-tab>
-    </x-slide-tab-container>
+    @fragment('taxa_list')
+        <div id="taxa_list">
+            @if(!empty(request('taxa')))
+            <h2 class="text-2xl font-bold" >
+                Select a species to access available media
+            </h2>
+            @elseif(request('target') === 'genus')
+            <h2 class="text-2xl font-bold" >
+                Select a genus to see species list
+            </h2>
+            @else
+            <h2 class="text-2xl font-bold" >
+                Select a family to see species list
+            </h2>
+            @endif
+            @foreach($taxa as $taxon)
+                <div>
+                    @if(!request('target') && !request('taxa'))
+                        <x-link href="{{ url()->current() }}?taxa={{ $taxon->name }}&target=genus">{{ strtoupper($taxon->name) }}</x-link>
+                    @elseif(isset($taxon->tid))
+                        <x-link href="{{ url('taxon/' . $taxon->tid) }}">{{ $taxon->name }}</x-link>
+                        <x-nav-link href="{{ url('media/search') }}?taxa={{ $taxon->name }}">
+                            <i class="text-xl fas fa-camera"></i>
+                        </x-nav-link>
+                    @else
+                        <x-link href="{{ url()->current() }}?taxa={{ $taxon->name }}">{{ $taxon->name }}</x-link>
+                    @endif
+                </div>
+            @endforeach
+        </div>
+    @endfragment
 </x-layout>
