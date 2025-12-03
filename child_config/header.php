414fd668b14d76149ea103450f54e10083dccb28
MuchQuak <loganpwilt@gmail.com>
2025-12-02 17:48:07 -0800

    Fix header render

--- child_config/header.php
+++ child_config/header.php
@@ -1,1 +1,11 @@
-<?= Blade::render('header') ?>
+<?= Blade::render('header', ['buttonVariant' => "primary" ]) ?>
+<?= Blade::render('navbar', ['navigations' => [
+    ["title" => __("header.home"), "link" => url('/'), "htmx" => true],
+    ["title" => __("header.collections"), "link" => url('/collections/search'), 'htmx' => true ],
+    ["title" => __("header.map_search"), "link" => legacy_url('/collections/map/index.php')],
+    ["title" => __("header.species_checklists"), "link" => url('/checklists'), 'htmx' => true],
+    ["title" => __("header.media"), "link" => url('/media/search'), 'htmx' => true],
+    ["title" => __("header.data_use"), "link" => url('/usagepolicy'), 'htmx' => true],
+    ["title" => __("header.symbiota_help"), "link" => docs_url()],
+    ["title" => __("header.sitemap"), "link" => url('/sitemap'), 'htmx' => true],
+]]) ?>
