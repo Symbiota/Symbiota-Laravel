5203b940fa6b6a5965f510b21f9e8b6d073c9832
MuchQuak <loganpwilt@gmail.com>
2025-12-17 13:01:55 -0800

    Add route, controller, and view working execept redirects

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -151,6 +151,8 @@ class MediaController extends Controller {
     }
 
     public static function profilePage(int $mediaID) {
-        return view('pages/media/profile', ['mediaID' => $mediaID]);
+        return view('pages/media/profile', [
+            'mediaID' => $mediaID
+        ]);
     }
 }
