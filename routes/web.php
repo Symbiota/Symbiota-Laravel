fae7306c711a6858a742fe6f410ceb412b608970
MuchQuak <loganpwilt@gmail.com>
2026-03-12 18:06:43 -0700

    fix tailwind link theming on markdown class

--- routes/web.php
+++ routes/web.php
@@ -61,7 +61,8 @@ Route::group(['prefix' => 'checklists'], function () {
     Route::get('/dynamicmap', [ChecklistController::class, 'dynamicMapPage']);
     Route::get('/map', [ChecklistController::class, 'mapPage']);
     Route::post('/create', [ChecklistController::class, 'createChecklist']);
-    Route::get('/{clid}/admin', [ChecklistController::class, 'getAdminPage']);
+    Route::match(['GET', 'POST'], '/{clid}/admin', [ChecklistController::class, 'getAdminPage']);
+    Route::get('/{clid}/pdf', [ChecklistController::class, 'browserPrint']);
     Route::get('/{clid}', [ChecklistController::class, 'checklist']);
 });
 
