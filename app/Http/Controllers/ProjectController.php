db73b7c40b205f28cdcf09f16f7d1672e6a718d9
MuchQuak <loganpwilt@gmail.com>
2026-03-16 17:29:15 -0700

    Rename EditProject Controller to projectAdmin and add in the legacy controller routes

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -27,7 +27,96 @@ class ProjectController extends Controller {
         return view('pages/project', self::getProjectData($pid));
     }
 
-    public static function EditProject(int $pid) {
+    public static function  create(int $pid) {
+        include_once(legacy_path('/classes/ImInventories.php'));
+        $projManager = new ImInventories('write');
+        $projManager->setPid($pid);
+
+	    // addNewProject
+		$pid = $projManager->insertProject(request()->all());
+		if(!$pid) $statusStr = $projManager->getErrorMessage();
+
+        return self::projectAdmin($pid);
+    }
+
+    public static function update(int $pid) {
+        include_once(legacy_path('/classes/ImInventories.php'));
+        $projManager = new ImInventories('write');
+        $projManager->setPid($pid);
+
+	    // submitEdit
+		$projManager->updateProject(request()->all());
+
+        return self::projectAdmin($pid);
+    }
+
+    public static function delete(int $pid) {
+        include_once(legacy_path('/classes/ImInventories.php'));
+        $projManager = new ImInventories('write');
+        $projManager->setPid($pid);
+
+	    // submitDelete
+        if(!$projManager->deleteProject(request('pid'))) {
+		    $statusStr = $projManager->getErrorMessage();
+        }
+
+        return self::projectAdmin($pid);
+    }
+
+    public static function removeUser(int $pid, int $uid) {
+        include_once(legacy_path('/classes/ImInventories.php'));
+        $projManager = new ImInventories('write');
+        $projManager->setPid($pid);
+
+	    // deluid
+		if(!$projManager->deleteUserRole('ProjAdmin', $pid, request('uid'))) {
+			$statusStr = $projManager->getErrorMessage();
+		}
+
+        return self::projectAdmin($pid);
+    }
+
+    public static function addUser(int $pid) {
+        include_once(legacy_path('/classes/ImInventories.php'));
+        $projManager = new ImInventories('write');
+        $projManager->setPid($pid);
+
+        //Add to Manager List
+        if(!$projManager->insertUserRole(request('uid'), UserRole::PROJ_ADMIN, 'fmprojects', $pid, request()->user()->uid)) {
+            $statusStr = $projManager->getErrorMessage();
+        }
+
+        return self::projectAdmin($pid);
+    }
+
+    public static function addChecklist(int $pid) {
+        include_once(legacy_path('/classes/ImInventories.php'));
+        $projManager = new ImInventories('write');
+        $projManager->setPid($pid);
+
+	    // Add Checklist
+		if(!$projManager->insertChecklistProjectLink(request('clid'))){
+			$statusStr = $projManager->getErrorMessage();
+		}
+
+        return self::projectAdmin($pid);
+    }
+
+    public static function removeChecklist(int $pid) {
+        include_once(legacy_path('/classes/ImInventories.php'));
+        $projManager = new ImInventories('write');
+        $projManager->setPid($pid);
+
+	    // Delete Checklist
+		if(!$projManager->deleteChecklistProjectLink(request('clid'))){
+			$statusStr = $projManager->getErrorMessage();
+		}
+
+        return self::projectAdmin($pid);
+    }
+
+    // rename to adminView
+    public static function projectAdmin(int $pid) {
         return view('pages/projects/edit', self::getProjectData($pid));
     }
 }
