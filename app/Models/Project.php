58893a86bcb86a58deb78d1160df558447989fc3
MuchQuak <loganpwilt@gmail.com>
2026-03-18 11:44:16 -0700

    Add project model

---
+++ app/Models/Project.php
@@ -0,0 +1,26 @@
+<?php
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
+
+class Project extends Model {
+    protected $table = 'fmprojects';
+    protected $primaryKey = 'pid';
+    protected $fillable = [
+        'projname',
+        'managers',
+        'fullDescription',
+        'notes',
+        'isPublic'
+    ];
+
+    public function checklists() {
+        return DB::table('fmchecklists as c')
+            ->select('link.pid', 'c.defaultSettings', 'c.clid', 'c.name', 'mapChecklist')
+            ->leftJoin('fmchklstprojlink as link', 'link.clid', '=', 'c.clid')
+            ->where('link.pid', '=', $this->pid)
+            ->orderByRaw('-link.pid DESC')
+            ->get();
+    }
+}
