63f31bd9a1e9dd0603fadb46f24158d44b8f2ee1
MuchQuak <loganpwilt@gmail.com>
2026-03-24 17:13:04 -0700

    Get the form edit and review flows working for toggling back and form

--- resources/views/core/pages/collections/trait-editor.blade.php
+++ resources/views/core/pages/collections/trait-editor.blade.php
@@ -2,9 +2,12 @@
 include_once(legacy_path('/classes/OccurrenceAttributes.php'));
 include_once(legacy_path('/classes/utilities/GeneralUtil.php'));
 
+$REVIEW = 1;
+$EDIT = 2;
+
 $collid = request('collid');
 $submitForm = request('submitform') ?? '';
-$mode = request('mode') ?? 1;
+$mode = request('mode') == $EDIT ? $EDIT: $REVIEW;
 $traitID = request('traitid') ?? '';
 $paneX = request('panex') ?? '575';
 $paneY = request('paney') ?? '550';
@@ -62,9 +65,7 @@ if ($traitID) {
 	}
 }
 
-$traitItems = itemize($attrManager->getTraitNames(), [
-    item('', __('editor_editreviewer.ALL_EDITORS'))
-]);
+$traitItems = itemize($attrManager->getTraitNames());
 
 $editorItems = itemize($attrManager->getEditorArr(), [
     item('', __('editor_editreviewer.ALL_EDITORS'))
@@ -92,9 +93,12 @@ $countryItems = itemize_flat($attrManager->getLocalFilterOptions(), [
     item('', __('traitattr_occurattributes.ALL_COUNTRIES_STATES'))
 ]);
 
+$traitArr = $attrManager->getTraitArr($traitID, ($mode == 2 ? true : false));
+
+
 @endphp
 
-<x-margin-layout x-data="{ mode: 0 }">
+<x-margin-layout x-data="{ mode: {{ $mode }} }">
     <x-breadcrumbs :items="[
         ['title' => __('header.H_HOME'), 'href' => url('') ],
         ['title' => __('traitattr_attributemining.COLLECTION_MANAGEMENT'), 'href' => url('collections/' . request('collid')) ],
@@ -118,53 +122,110 @@ $countryItems = itemize_flat($attrManager->getLocalFilterOptions(), [
 
     <hr/>
 
-	<fieldset x-cloak x-show="mode === 0">
-		<legend class="text-lg font-bold">
-            {{ __('traitattr_occurattributes.REVIEWER') }}
-        </legend>
-		<form id="reviewform" class="flex flex-col gap-4" name="reviewform" method="post">
-            @csrf
-            <div class="flex flex-wrap gap-2">
-                <x-select class="flex-grow" id="traitid" :items="$traitItems"
+    @if($mode === $EDIT)
+    <x-accordion :label="__('misc_sharedterms.FILTER')" :open="true">
+        <fieldset>
+            <legend class="text-lg font-bold">
+                {{ __('misc_sharedterms.FILTER') }}
+            </legend>
+            <form id="filterform" class="flex flex-col gap-4" name="filterform" method="post">
+                @csrf
+                <x-select class="w-full flex-grow" id="traitid"
+                    :defaultValue="$traitID"
+                    :items="$traitItems"
                     :select_text="__('traitattr_occurattributes.SELECT_TRAIT_REQ')"
                 />
-
-                <x-select class="w-auto flex-grow" id="reviewuid" default="0" :items="$editorItems"/>
-                <x-select class="w-auto flex-grow" id="reviewdate" default="0" :items="$dateItems"/>
-                <x-select class="w-auto flex-grow" id="reviewstatus" default="0" :items="$reviewStatusItems"/>
-                <x-select class="w-auto flex-grow" id="sourcefilter" default="0" :items="$sourceItems"/>
-                <x-select class="w-auto flex-grow" id="localfilter" default="0" :items="$countryItems"/>
+                <div class="flex flex-wrap gap-4">
+                    <x-select class="w-auto flex-grow" id="localfilter" default="0" :items="$countryItems"/>
                 </div>
-            <x-taxa-search />
-            <input type="hidden" name="submitForm" value="Get Images">
-            <x-button>{{ __('traitattr_occurattributes.GET_IMAGES') }}</x-button>
-        </form>
-    </fieldset>
+                <x-taxa-search />
+                <input type="hidden" name="mode" value="{{ $EDIT }}" />
+                <input type="hidden" name="submitForm" value="Load Images">
+                <input id="panex1" name="panex" type="hidden" value="{{ $paneX }}" />
+                <input id="paney1" name="paney" type="hidden" value="{{ $paneY }}" />
+                <input id="imgres1" name="imgres" type="hidden" value="{{ $imgRes }}" />
+
+                <x-button>{{ __('traitattr_occurattributes.GET_IMAGES') }}</x-button>
+            </form>
+        </fieldset>
+    </x-accordion>
+    @else
+    <x-accordion :label="__('traitattr_occurattributes.REVIEWER')" :open="true">
+        <fieldset>
+            <legend class="text-lg font-bold">
+                {{ __('traitattr_occurattributes.REVIEWER') }}
+            </legend>
+            <form id="reviewform" class="flex flex-col gap-4" name="reviewform" method="post">
+                @csrf
+
+                <x-select class="w-full flex-grow" default="0" id="traitid" :items="$traitItems"
+                    :select_text="__('traitattr_occurattributes.SELECT_TRAIT_REQ')"
+                />
+                <div class="flex flex-wrap gap-4">
+                    <x-select class="min-w-60 w-auto flex-grow" id="reviewuid" default="0" :items="$editorItems"/>
+                    <x-select class="w-auto flex-grow" id="reviewdate" default="0" :items="$dateItems"/>
+                    <x-select class="w-auto flex-grow" id="reviewstatus" default="0" :items="$reviewStatusItems"/>
+                    <x-select class="w-auto flex-grow" id="sourcefilter" default="0" :items="$sourceItems"/>
+                    <x-select class="w-auto flex-grow" id="localfilter" default="0" :items="$countryItems"/>
+                    </div>
+                <x-taxa-search />
+                <input type="hidden" name="mode" value="{{ $EDIT }}" />
+                <input type="hidden" name="submitForm" value="Get Images">
+                <x-button>{{ __('traitattr_occurattributes.GET_IMAGES') }}</x-button>
+            </form>
+        </fieldset>
+    </x-accordion>
+    @endif
 
     <hr/>
 
-	<fieldset x-cloak x-show="mode === 1">
-		<legend class="font-bold">
-            {{ __('traitattr_occurattributes.FILTER') }}
-        </legend>
-		<form id="filterform" name="filterform" method="post">
-
-        </form>
-    </fieldset>
-
     @if(!empty($imgArr))
-    <x-radio name="resradio" :options="[
-        [ 'value' => 'high', 'label' => __('traitattr_occurattributes.HIGH_RES') ],
-        [ 'value' => 'med', 'label' => __('traitattr_occurattributes.MED_RES') ],
-    ]" />
+    <div class="flex items-center gap-2">
+        <x-radio class="m-0" name="resradio" :options="[
+            [ 'value' => 'high', 'label' => __('traitattr_occurattributes.HIGH_RES') ],
+            [ 'value' => 'med', 'label' => __('traitattr_occurattributes.MED_RES') ],
+        ]" />
+        <span class="flex-grow">
+            <x-link href="{{ url('occurrence/' . $occid) }}">{{ $catNum ?? 'specimen details' }}</x-link>
+        </span>
+        <x-button @click="document.getElementById('filterform').submit()">{{ __('traitattr_occurattributes.SKIP')}} >></x-button>
+    </div>
 
+
+    <form class="flex gap-2">
     @foreach ($imgArr as $image)
-        <div class="w-50 h-50 bg-base-300">
-            <img class="w-50 h-50" src="{{ $image['web'] ?? $image['lg'] }}" loading="lazy" />
+        <div class="mx-auto w-150 h-150 bg-base-300">
+            <img class="w-150 h-150" src="{{ $image['web'] ?? $image['lg'] }}" loading="lazy" />
+        </div>
+
+        <div class="border border-base-300 flex-grow p-4 flex flex-col gap-4">
+            <div class="text-lg font-bold">Trait Name</div>
+            <div>
+                {!! $attrManager->echoFormTraits($traitID) !!}
+            </div>
+            <x-input id="notes" :label="__('projects.NOTES')" />
+            <x-select class="w-full" id="status"
+                :label="__('taxonomy_batchloader.STATUS')"
+                :items="$editStatusItems"
+            />
+
+            @if($mode == $EDIT)
+            <input type="hidden" name="submitForm" value="Set Status and Save">
+            <x-button>
+                {{ __('traitattr_occurattributes.SET_STATUS_SAVE') }}
+            </x-button>
+            @else
+            <input type="hidden" name="submitForm" value="Save and Next">
+            {{-- TODO (Logan) toggle logic fro when this is not disabled --}}
+            <x-button :disabled="true">
+                {{ __('traitattr_occurattributes.SAVE_NEXT') }}
+            </x-button>
+            @endif
         </div>
     @endforeach
+    </form>
 
-    @else
+    @elseif($submitForm)
     <div class="font-bold">
         {{ __('traitattr_occurattributes.NO_IMAGES_MATCHING_CRITERIA') }}
     </div>
