a5667fa7a9f3cb7044c2c71694355b8646a2d4f8
MuchQuak <loganpwilt@gmail.com>
2026-03-27 15:14:10 -0700

    Move checklist metadata component to laravel lang tags

--- resources/views/core/checklist/metadata.blade.php
+++ resources/views/core/checklist/metadata.blade.php
@@ -1,17 +1,16 @@
 @props(['checklist', 'parent' => [], 'children' => [], 'exclusions' => []])
-@php global $LANG @endphp
 
 @if($checklist->type === 'rarespp')
 {{-- TODO --}}
 @elseif($checklist->type === 'excludespp' && $parent && count($parent) > 0)
-<x-text-label :label="$LANG['EXCLUSION_LIST']">
+<x-text-label :label="__('checklists_checklist.EXCLUSION_LIST')">
     <x-link href="{{ url('checklists/' . key($parent)) }}">
         {{ current($parent) }}
     </x-link>
 </x-text-label>
 @endif
 
-<x-text-label :label="$LANG['INCLUDE_TAXA']">
+<x-text-label :label="__('checklists_checklist.INCLUDE_TAXA')">
 @foreach($children as $clid => $name)
     <div class="pl-4">
         <li><x-link href="{{ url('checklists/'. $clid)}}">
@@ -21,7 +20,7 @@
 @endforeach
 </x-text-label>
 
-<x-text-label :label="$LANG['TAXA_EXCLUDED']">
+<x-text-label :label="__('checklists_checklist.TAXA_EXCLUDED')">
 @foreach($exclusions as $clid => $name)
     <div class="pl-4">
         <li><x-link href="{{ url('checklists/'. $clid)}}">
@@ -32,13 +31,13 @@
 </x-text-label>
 
 @foreach ([
-    $LANG['AUTHORS'] => $checklist->authors ?? false,
-    $LANG['CITATION'] => $checklist->publication ?? false,
+    __('checklists_checklist.AUTHORS') => $checklist->authors ?? false,
+    __('checklists_checklist.CITATION') => $checklist->publication ?? false,
 ] as $label => $value)
     <x-text-label :label="$label">{{ $value }}</x-text-label>
 @endforeach
 @if($checklist->type !== 'excludespp')
-    <x-text-label :label="$LANG['LOCALITY']">
+    <x-text-label :label="__('imagelib_imgdetails.LOCALITY')">
         @isset($checklist->locality)
         {{ $checklist->locality }}
         @endisset
@@ -47,7 +46,7 @@
         @endif
     </x-text-label>
 @endif
-<x-text-label class="markdown" :label="$checklist->type == 'excludespp'? $LANG['COMMENTS']: $LANG['ABSTRACT']">
+<x-text-label class="markdown" :label="$checklist->type == 'excludespp'? __('checklists_checklist.COMMENTS'): __('checklists_checklist.ABSTRACT')">
     {!! Purify::clean($checklist->abstract) !!}
 </x-text-label>
-<x-text-label :label="$LANG['NOTES']">{{ $checklist->notes }}</x-text-label>
+<x-text-label :label="__('projects.NOTES')">{{ $checklist->notes }}</x-text-label>
