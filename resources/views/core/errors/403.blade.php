e53228f287b27e12d17b19280ca63e66ebc02b7e
MuchQuak <loganpwilt@gmail.com>
2026-03-31 12:55:20 -0700

    Add 403 error translations

--- resources/views/core/errors/403.blade.php
+++ resources/views/core/errors/403.blade.php
@@ -1,33 +1,35 @@
 @php
-global $LANG;
-include_once(legacy_path('/classes/utilities/Language.php'));
-Language::load('prohibit');
 $referer = request()->header('referer');
 $hasLogin = auth()->check();
 @endphp
 
-{{-- TODO (Logan) translation for text before pr merge (waiting on translation changes) --}}
 <x-margin-layout class="justify-center items-center" :hasHeader="false" :hasNavbar="false" :hasFooter="false">
     <div class="flex flex-col gap-4 p-4 border border-base-300 rounded-md">
         <div>
-            <h1 class="text-4xl font-bold">Resource Access Denied</h1>
+            <h1 class="text-4xl font-bold">{{ __('Resource Access Denied') }}</h1>
             <hr>
         </div>
         @if(!$hasLogin)
-        <p>You must be logged in to access this page</p>
+        <p>{{ __("You must be logged in to access this page") }}</p>
         @else
-        <p>{{ $LANG['NO_PERMISSION']}}</p>
+        <p>{{ __("You don't have permission to access this page") }}</p>
         @endif
 
         <div class="flex items-center gap-2 flex-wrap">
             @if(!$hasLogin)
-            <x-button class="flex-grow justify-center" :href="url('/login')">Login</x-button>
+            <x-button class="flex-grow justify-center" :href="url('/login')">
+                {{ __('header.H_LOGIN') }}
+            </x-button>
             @else
-            <x-button class="flex-grow justify-center" :href="url('/')">Return to home</x-button>
+            <x-button class="flex-grow justify-center" :href="url('/')">
+                {{ __('Return to homepage') }}
+            </x-button>
             @endif
 
             @if($referer && $referer !== url()->current())
-            <x-button class="flex-grow justify-center"  :href="$referer">Go back</x-button>
+            <x-button class="flex-grow justify-center"  :href="$referer">
+                {{ __('Go back') }}
+            </x-button>
             @endif
         </div>
     </div>
