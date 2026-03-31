33830ec87ec828435cb9a2c52a1181c61bce0cb6
atticus29 <mark.aaron.fisher@gmail.com>
2026-03-31 15:16:48 -0700

    rever to main version of resources/js/app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,15 @@
-import Alpine from "alpinejs";
-import focus from "@alpinejs/focus";
-import htmx from "htmx.org";
-import L from "leaflet";
-import "leaflet-draw";
-import "leaflet.markercluster";
-import * as editor from "./editor";
-import "tinymce/icons/default/icons.min.js";
-import "tinymce/themes/silver/theme.min.js";
-import "tinymce/models/dom/model.min.js";
-import "./components/autocomplete-input";
-import "./components/taxonomy-create";
-import { Chart, registerables } from "chart.js";
+import Alpine from 'alpinejs';
+import focus from '@alpinejs/focus'
+import htmx from 'htmx.org';
+import L from 'leaflet';
+import 'leaflet-draw';
+import 'leaflet.markercluster';
+import * as editor from './editor';
+import 'tinymce/icons/default/icons.min.js';
+import 'tinymce/themes/silver/theme.min.js';
+import 'tinymce/models/dom/model.min.js';
+import './components/autocomplete-input';
+import { Chart, registerables } from 'chart.js';
 
 Chart.register(...registerables);
 window.Chart = Chart;
@@ -28,12 +27,11 @@ window.Alpine = Alpine;
 Alpine.plugin(focus);
 
 //Custom Functions
-function openWindow(
-    link = "",
-    title = "",
-    options = "resizable=0,width=900,height=630,left=20,top=20",
-) {
-    let mapWindow = open(link, title, options);
+function openWindow(link = "", title = "", options = "resizable=0,width=900,height=630,left=20,top=20") {
+    let mapWindow = open(link,
+        title,
+        options,
+    );
     if (mapWindow.opener == null) mapWindow.opener = self;
     mapWindow.focus();
 }
@@ -41,7 +39,7 @@ function openWindow(
 window.openWindow = openWindow;
 
 function copyUrl(urlOverride) {
-    const url = urlOverride ? urlOverride : window.location;
+    const url = urlOverride ? urlOverride: window.location;
     const type = "text/plain";
 
     const clipboardItemData = {
@@ -52,12 +50,12 @@ function copyUrl(urlOverride) {
 
     //Assumes toaster is setup
     navigator.clipboard.write([clipboardItem]).then(
-        (res) => window.toast("Url Copied!", { type: "success" }),
-        (error) => window.toast("Failed to Copy Url!", { type: "danger" }),
+        res => window.toast('Url Copied!', {type: 'success'}),
+        error => window.toast('Failed to Copy Url!', {type: 'danger'})
     );
 }
 window.copyUrl = copyUrl;
 
 queueMicrotask(() => {
-    Alpine.start();
+    Alpine.start()
 });
