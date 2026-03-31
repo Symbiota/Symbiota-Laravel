709e96ff2df58499655a66191b15cebe5fb53d37
MuchQuak <loganpwilt@gmail.com>
2026-03-31 13:19:17 -0700

    Add translations and fieldsets for add Project form

--- resources/views/core/pages/projects/create.blade.php
+++ resources/views/core/pages/projects/create.blade.php
@@ -1,31 +1,23 @@
 @php
-global $LANG, $IS_KEY_MOD_IS_ACTIVE;
-include_once(legacy_path('/classes/utilities/Language.php'));
 include_once(legacy_path('/classes/ImInventories.php'));
-
-Language::load([
-    'projects/index',
-    'checklists/index'
-]);
-
 $projManager = new ImInventories('write');
 @endphp
 
 <x-margin-layout>
     <div>
         <x-breadcrumbs :items="[
-        ['title' => $LANG['NAV_HOME'], 'href' => url('') ],
-        ['title' => $LANG['SPECIES_INVENTORIES'], 'href' => url('/checklists') ],
-        /* TODO (Logan) translate */
-        'Create a Project'
-    ]" />
-    </div>
-    <div>
-        <h3 class="text-4xl font-bold text-primary">{{ $LANG['ADD_NEW'] }}</h3>
-    </div>
+        ['title' => __('header.H_HOME'), 'href' => url('') ],
+        ['title' => __('checklists.SPECIES_INVENTORIES'), 'href' => url('/checklists') ],
+        __('projects.ADDNEWPR')
+    ]" /> </div>
+    <fieldset class="flex flex-col gap-4">
+        <div>
+            <h3 class="text-4xl font-bold text-primary">{{ __('projects.ADDNEWPR') }}</h3>
+        </div>
 
-    <form class="flex flex-col gap-4" hx-post="{{ url()->current() }}" method="post" x-on:hx-after-response="console.log(event)" >
-        <x-projects.form />
-        <x-button>{{ $LANG['ADDNEWPR'] }}</x-button>
-    </form>
+        <form class="flex flex-col gap-4" hx-post="{{ url()->current() }}" method="post">
+            <x-projects.form />
+            <x-button>{{ __('projects.ADDNEWPR') }}</x-button>
+        </form>
+    </fieldset>
 </x-margin-layout>
