201c20db9b8f2514bff1bab8b9c46b56932201e4
Logan Wilt <91149982+MuchQuak@users.noreply.github.com>
2026-04-03 10:33:38 -0700

    Add Translations to coll profile list (#114)
    * Add Translations to coll profile list
    
    * Pint fixies
    
    * remove controller to fix fetch all and view for simplicity

--- resources/views/core/pages/collections/profile-list.blade.php
+++ resources/views/core/pages/collections/profile-list.blade.php
@@ -1,16 +1,17 @@
-@props(['collections'])
+@props(['collections' => \App\Models\Collection::query()->get()])
 <x-layout class="flex flex-col gap-4">
     <x-breadcrumbs :items="[
-        ['title' => 'Home', 'href' => url('')],
-        ['title' => 'Collection Search', 'href' => url('collections/search'), ],
-        ['title' => 'Collection Profiles']
+        ['title' => __('header.H_HOME'), 'href' => url('')],
+        ['title' => __('misc_collprofiles.COLLECTION_SEARCH'), 'href' => url('collections/search'), ],
+        ['title' => __('misc_collmetadata.COL_PROFS')]
         ]" />
     <div class="text-4xl font-bold">
-        Natural History Collections and Observation Projects
+        {{ __('misc_collprofiles.COLLECTION_PROJECTS') }}
     </div>
 
-    <x-link target="_blank" href="{{ legacy_url('/collections/datasets/rsshandler.php') }}">RSS
-        Feed</x-link>
+    <x-link target="_blank" href="{{ legacy_url('/collections/datasets/rsshandler.php') }}">
+        {{ __('misc_collprofiles.RSS_FEED') }}
+    </x-link>
 
     @foreach ($collections as $collection)
     <div class="border border-base-300 p-4">
@@ -24,7 +25,9 @@
         <div>{!! Purify::clean($collection->fullDescription) !!}</div>
 
         <div>
-            <x-link href="{{ $collection->homepage}}">Homepage</x-link>
+            <x-link href="{{ $collection->homepage}}">
+                {{ __('misc_collmetadata.HOMEPAGE') }}
+            </x-link>
         </div>
 
         <div>
@@ -35,7 +38,9 @@
                 @foreach($contacts as $contact)
                     @if(isset($contact['firstName']) && isset($contact['lastName']) && isset($contact['email']))
                     <div>
-                        <span class="font-bold">{{ $contact['role'] ?? 'Contact' }}:</span>
+                        <span class="font-bold">
+                            {{ $contact['role'] ?? __('misc_collmetadata.CONTACT') }}:
+                        </span>
                         {{ $contact['firstName'] . ' ' . $contact['lastName'] . ' ' . $contact['email'] }}
                     </div>
                     @endif
@@ -43,7 +48,9 @@
             @endif
         </div>
 
-        <x-link href="{{ url('collections/' . $collection->collID) }}">More Information</x-link>
+        <x-link hx-boost="true" href="{{ url('collections/' . $collection->collID) }}">
+            {{ __('header.H_MORE_INFO') }}
+        </x-link>
     </div>
     @endforeach
 </x-layout>
