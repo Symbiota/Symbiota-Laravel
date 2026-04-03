2e1d4652ba092fc1da70daff3b657d0a970f9049
atticus29 <mark.aaron.fisher@gmail.com>
2026-04-02 17:38:48 -0700

    add style improvements to tree-node

--- resources/views/core/tree-node.blade.php
+++ resources/views/core/tree-node.blade.php
@@ -2,13 +2,18 @@
 <ul>
     @foreach($nodes as $node)
         <li x-data="{ open: false }">
-            <div @click="open = !open">
-                {{ $node->sciName }}
-            </div>
             @if($node->children ?? false)
-                <div class="flex flex-row gap-2">
-                    <span>--->
-                    </span>
+                <div @click="open = !open">
+                    <x-link>{{ $node->sciName }}+</x-link>
+                </div>
+            @endif
+            @if(!($node->children ?? false))
+                <div class="pl-2">
+                    <x-link href="{{url('taxon/' . $node->tid)}}">{{ $node->sciName }}</x-link>
+                </div>
+            @endif
+            @if($node->children ?? false)
+                <div class="pl-2">
                     <ul x-show="open">
                         <x-tree-node :nodes="$node->children ?? []" />
                     </ul>
