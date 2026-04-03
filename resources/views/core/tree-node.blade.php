193d0a96c0b3396430a4ea63bc33cb9a588d0728
atticus29 <mark.aaron.fisher@gmail.com>
2026-04-02 17:29:33 -0700

    fix tree-node component

--- resources/views/core/tree-node.blade.php
+++ resources/views/core/tree-node.blade.php
@@ -3,12 +3,17 @@
     @foreach($nodes as $node)
         <li x-data="{ open: false }">
             <div @click="open = !open">
-                {{ $node->name }}
+                {{ $node->sciName }}
             </div>
-
-            <ul x-show="open">
-                @include('tree-node', ['nodes' => $node->children])
-            </ul>
+            @if($node->children ?? false)
+                <div class="flex flex-row gap-2">
+                    <span>--->
+                    </span>
+                    <ul x-show="open">
+                        <x-tree-node :nodes="$node->children ?? []" />
+                    </ul>
+                </div>
+            @endif
         </li>
     @endforeach
 </ul>
