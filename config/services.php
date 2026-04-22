90a17dfd811280b7c3d49503d9470b3da13c7eb4
MuchQuak <loganpwilt@gmail.com>
2026-04-22 14:53:49 -0700

    rework orcid oauth to use app url so that the https scheme is used when server is https

--- config/services.php
+++ config/services.php
@@ -33,6 +33,6 @@ return [
     'orcid' => [
         'client_id' => env('ORCID_CLIENT_ID'),
         'client_secret' => env('ORCID_CLIENT_SECRET'),
-        'redirect' => url('/auth/oauth/orcid'),
+        'redirect' => env('APP_URL') .  '/auth/oauth/orcid',
     ],
 ];
