551f620e3f8e2fa4ebd77ec3466f4ccea5e1e3c8
MuchQuak <loganpwilt@gmail.com>
2026-04-22 12:09:09 -0700

    force https when production or staging

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,6 +10,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Event;
 use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\View\Compilers\BladeCompiler;
 
@@ -23,6 +24,9 @@ class AppServiceProvider extends ServiceProvider {
      * Bootstrap any application services.
      */
     public function boot(): void {
+        URL::forceHttps(
+            $this->app->environment(['staging', 'production'])
+        );
         /**
          * Define Authorization Gates
          */
