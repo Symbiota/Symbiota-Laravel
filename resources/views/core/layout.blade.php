874bf1e3555e32a43e9af27672fa88fafdf99ad6
MuchQuak <loganpwilt@gmail.com>
2026-04-23 12:49:43 -0700

    fix load odder so alpine inits happen after libraries have loaded

--- resources/views/core/layout.blade.php
+++ resources/views/core/layout.blade.php
@@ -44,11 +44,11 @@ $grants= [
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite([
-        'resources/js/app.js',
         'resources/js/htmx.js',
         'resources/js/editor.js',
         'resources/js/leaflet.js',
         'resources/js/chart.js',
+        'resources/js/app.js',
         'resources/css/app.css'
     ])
     @stack('head')
