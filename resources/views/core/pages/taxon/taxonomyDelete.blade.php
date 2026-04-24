448dc7f57a522e6c85b63cf396acf6b0f5fe7cef
atticus29 <mark.aaron.fisher@gmail.com>
2026-04-23 17:52:27 -0700

    continue implementing the various taxonomy deletion validations

--- resources/views/core/pages/taxon/taxonomyDelete.blade.php
+++ resources/views/core/pages/taxon/taxonomyDelete.blade.php
@@ -6,4 +6,15 @@
 @php
     
 @endphp
+<x-taxon-linked-item :items="$verifyArr['child'] ?? []" title="Child Taxa" warning="Warning: children taxa exist for this taxon. They must be remapped before this taxon can be removed." item-name-plural="child taxa" />
 <x-taxon-linked-item :items="$verifyArr['syn'] ?? []" title="Synonym Links" warning="Warning: synonym links exist for this taxon. They must be remapped before this taxon can be removed." item-name-plural="synonym links" />
+<x-taxon-linked-item :items="$verifyArr['img'] ?? []" title="Image Links" warning="Warning: images exist for this taxon. They must be remapped before this taxon can be removed." item-name-plural="images" />
+<x-taxon-linked-item :items="$verifyArr['map'] ?? []" title="Taxon Maps" warning="Warning: taxon maps exist for this taxon. They must be remapped before this taxon can be removed." item-name-plural="taxon maps" />
+<x-taxon-linked-item :items="$verifyArr['vern'] ?? []" title="Vernaculars" warning="Warning: vernacular names exist for this taxon. They must be remapped before this taxon can be removed." item-name-plural="vernacular names" />
+<x-taxon-linked-item :items="$verifyArr['tdesc'] ?? []" title="Text Descriptions" warning="Warning: text descriptions exist for this taxon. They must be remapped before this taxon can be removed." item-name-plural="linked text descriptions" />
+<x-taxon-linked-item :items="$verifyArr['occur'] ?? []" title="Occurrence Records" warning="Warning: occurrence records exist for this taxon. They must be remapped before this taxon can be removed." item-name-plural="linked occurrence records" />
+<x-taxon-linked-item :items="$verifyArr['dets'] ?? []" title="Determinations" warning="Warning: determinations exist for this taxon. They must be remapped before this taxon can be removed." item-name-plural="linked determination records" />
+<x-taxon-linked-item :items="$verifyArr['cl'] ?? []" title="Checklists" warning="Warning: checklists exist for this taxon. They must be remapped before this taxon can be removed." item-name-plural="linked checklists" />
+<x-taxon-linked-item :items="$verifyArr['kmdesc'] ?? []" title="Morphological Character Key Descriptions" warning="Warning: morphological characters exist for this taxon. They must be remapped before this taxon can be removed." item-name-plural="linked morphological characters" />
+<x-taxon-linked-item :items="$verifyArr['link'] ?? []" title="Linked Resources" warning="Warning: linked resources exist for this taxon. They must be remapped before this taxon can be removed." item-name-plural="linked resources" />
+
--- resources/views/core/taxon-linked-item.blade.php
+++ resources/views/core/taxon-linked-item.blade.php
@@ -1,12 +1,20 @@
-@props(['title'=>'Taxon-linked item', 'warning'=>'Warning!', items=>[], 'itemNamePlural'=>'items'])
+@props(['title'=>'Taxon-linked item', 'warning'=>'Warning!', 'items'=>[], 'itemNamePlural'=>'items'])
+@php $itemCount = is_countable($items) ? count($items) : (int)$items; @endphp
 <span class="font-bold text-lg">{{ $title }}</span>
-@if($items->count() > 0)
-    <p class="mt-2">{{ $warning }}</p>
-    <ul class="list-disc list-inside mt-2">
-        @foreach($items as $item)
-            <li><x-link :href="$item->url">{{ $item->name }}</x-link></li>
-        @endforeach
+@if($itemCount > 0)
+    <p class="mt-2 text-error-darker">{{ $warning }}</p>
+    @if(is_countable($items))
+        <ul class="list-disc list-inside mt-2">
+            @foreach($items as $item)
+                @php
+                    $itemUrl = is_array($item) ? $item['url'] : (is_string($item) ? $item : $item->url);
+                    $itemName = is_array($item) ? $item['name'] : (is_string($item) ? $item : $item->name);
+                @endphp
+                <li><x-link :href="$itemUrl">{{ $itemName }}</x-link></li>
+            @endforeach
+        </ul>
+    @endif
 @endif
-@if($items->count() < 1)
-    <p class="mt-2">Approved: No {{ $itemNamePlural }} linked to this taxon.</p>
+@if($itemCount < 1)
+    <p class="mt-2 text-accent-darker">Approved: No {{ $itemNamePlural }} linked to this taxon.</p>
 @endif
