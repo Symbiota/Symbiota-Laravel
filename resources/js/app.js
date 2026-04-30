80d16c26a810b11c81386c85bee072addc8e2360
atticus29 <mark.aaron.fisher@gmail.com>
2026-04-30 11:17:48 -0700

    add mains version of app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,19 +4,6 @@ import './components/autocomplete-input';
 import './components/taxonomy-create';
 import './components/toaster';
 import './helpers';
-import { Chart, registerables } from 'chart.js';
-
-Chart.register(...registerables);
-window.Chart = Chart;
-
-window.tinymce_editor = editor;
-
-window.L = L;
-window.type = true;
-// See Github https://github.com/Leaflet/Leaflet.draw/issues/1013 for context
-window.radius = undefined;
-
-window.htmx = htmx;
 
 window.Alpine = Alpine;
 
