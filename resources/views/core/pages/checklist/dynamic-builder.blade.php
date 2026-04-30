23a3469d18affe2d26b0e1dafb13cd8eeeeb8ead
Logan Wilt <91149982+MuchQuak@users.noreply.github.com>
2026-04-30 13:21:06 -0700

    Blitz dynchecklistmap (#110)
    * Add Dynamic Checklist Builder
    
    * clean up includes and language translations
    
    * Pint fixes
    
    * Fix error handler
    
    * pintfixes
    
    * add links to new url
    
    * Cleanup language tags

--- resources/views/core/pages/checklist/dynamic-builder.blade.php
+++ resources/views/core/pages/checklist/dynamic-builder.blade.php
@@ -1,28 +1,102 @@
-<x-layout class="flex flex-col gap-4">
+@props(['errors'])
+<x-margin-layout>
     <x-breadcrumbs :items="[
-        ['title' => 'Home', 'href' => url('')],
-        ['title' => 'Dynamic Map'],
-        ]" />
-    <div>
-        Description
-    </div>
-
-    <div>
-        Point (Lat, Long) (click on map)
-    </div>
-
-    <x-taxa-search label="Taxon Filter" />
-    <div>
-        <x-button>Build Checklist</x-button>
-    </div>
-
-    <div class="flex items-center gap-2">
-        <x-input name="radius" />
-        <x-select name="units" :items="[
-        ['title' => 'Kilometers', 'value' => 'kilometers' ],
-        ['title' => 'Miles', 'value' => 'miles' ]
+        ['title' => __('header.H_HOME'), 'href' => url('')],
+        ['title' => __('checklists_checklist.DYNAMIC_MAP')],
     ]" />
-    </div>
 
-    <x-map />
-</x-layout>
+    <h1 class="sr-only">{{ __('checklists_dynamicmap.DYNAMIC_MAP') }}></h1>
+
+    <div class="flex flex-col gap-1" x-data="{'moreDetail': false}">
+        <p>
+            {{ __('checklists_dynamicmap.CAPTURE_COORDS') }}
+        <p/>
+
+        <p x-show="moreDetail">
+            {{ __('checklists_dynamicmap.RADIUS_DESCRIPTION') }}
+        <p/>
+
+        <x-button class="cursor-pointer" @click="moreDetail=!moreDetail">
+            <span x-show="moreDetail">
+                {{ __('checklists_dynamicmap.LESS_DETAILS') }}
+            </span>
+            <span x-show="!moreDetail">
+                {{ __('checklists_dynamicmap.MORE_DETAILS') }}
+            </span>
+        </x-button>
+    </div>
+    <form method="post" class="flex flex-col gap-4">
+        @csrf
+        <div class="flex flex-wrap items-center gap-2">
+            <input type="hidden" name="interface" value="{{ request('interface') ?? 'checklist' }}" />
+            <input type="hidden" name="buildChecklist" value="1" />
+            <div class="min-w-20 max-w-40 flex-grow"><x-input id="lat" :label="__('tools_mapaids.MPR_LAT')" value=""/></div>
+            <div class="min-w-20 max-w-40 flex-grow"><x-input id="lng" :label="__('tools_mapaids.MPR_LNG')" value=""/></div>
+            <div class="min-w-20 max-w-40 flex-grow"><x-input id="radius" :label="__('checklists_dynamicmap.RADIUS')" type="number" /></div>
+            <x-select class="min-w-10 flex-grow" id="radiusunits" :label="__('checklists_dynamicmap.UNITS')" default="0" :items="[
+                item('km', __('checklists_dynamicmap.KM')),
+                item('mi', __('checklists_dynamicmap.MILES'))
+            ]"/>
+        </div>
+
+        <x-taxa-search class="z-100" :label="__('checklists_dynamicmap.TAXON_FILTER')" />
+
+        <div>
+            <x-button>{{ __('checklists_dynamicmap.BUILD_CHECKLIST') }}</x-button>
+        </div>
+
+        <x-errors :errors="$errors" />
+    </form>
+
+    <script>
+        document.addEventListener('mapIntialized', function (e) {
+            let map = window.maps['map'];
+            let markerGroup = new L.layerGroup().addTo(map);
+            let latlng;
+
+            const radiusEl = document.getElementById('radius');
+            const radiusUnitsEl = document.getElementById('radiusunits');
+            const latEl= document.getElementById('lat');
+            const lngEl= document.getElementById('lng');
+
+            function getRadius() {
+                if(radiusUnitsEl.value === "km") return radiusEl.value * 1000;
+                const MILES_TO_METERS = 1609.344;
+                return radiusEl.value * MILES_TO_METERS;
+            }
+
+            function drawMarker(lat, lng) {
+                //Clear Layers In Between Clicks
+                if(markerGroup) markerGroup.clearLayers();
+
+                lat = lat = parseFloat(lat);
+                lng = lng = parseFloat(lng);
+
+                if((lat >= -90 || lat <= 90) && (lng >= -180 || lng <= 180)) {
+                    latlng = {lat, lng};
+                    //Render Marker
+                    L.marker(latlng).addTo(markerGroup);
+                    let radius = getRadius();
+                    //Render Radius if Input
+                    if(radius > 0) {
+                        let circle = L.circle(latlng, radius)
+                            .setStyle(DEFAULT_SHAPE_OPTIONS)
+                            .addTo(markerGroup);
+                    }
+
+                    map.setView(latlng, map.getZoom());
+                }
+            }
+
+            radiusEl.addEventListener('input', () => drawMarker(latlng.lat, latlng.lng));
+            latEl.addEventListener('change', (e) => drawMarker(e.target.value, lngEl.value));
+            lngEl.addEventListener('change', (e) => drawMarker(latEl.value, e.target.value));
+            map.on('click', (e) => {
+                drawMarker(e.latlng.lat, e.latlng.lng)
+                latEl.value = e.latlng.lat;
+                lngEl.value = e.latlng.lng;
+            });
+        })
+    </script>
+    <x-map class="z-0"/>
+</x-margin-layout>
