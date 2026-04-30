3a313b5135539d3e59c78c8a7a33e6e042b20ac5
MuchQuak <loganpwilt@gmail.com>
2026-04-30 13:06:23 -0700

    remove comment

--- routes/web.php
+++ routes/web.php
@@ -165,7 +165,6 @@ Route::group(['prefix' => '/collections'], function () {
     Route::post('/{collid}/skeletal', [CollectionController::class, 'skeletalAdd'])->can('COLL_EDIT', 'collid');
     Route::get('/{collid}', [CollectionController::class, 'collection']);
     Route::match(['GET', 'POST'], '/{collid}/comments', [CollectionController::class, 'comments'])->can('COLL_ADMIN', 'collid')->where('collid', '[0-9+]');
-    // Route::match(['GET', 'POST'], '/{collid}/traits/edit', [CollectionTraitController::class, 'editor'])->can('COLL_EDIT', 'collid');
 
     Route::controller(CollectionTraitController::class)->group(function () {
         Route::get('/{collid}/traits/edit', 'editor')->can('COLL_EDIT', 'collid');
