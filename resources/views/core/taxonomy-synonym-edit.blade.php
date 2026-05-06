9aa2819f47753ea98b9a8f41a14c2903b4c086a4
atticus29 <mark.aaron.fisher@gmail.com>
2026-05-05 23:12:41 -0700

    add forgotten file

--- resources/views/core/taxonomy-synonym-edit.blade.php
+++ resources/views/core/taxonomy-synonym-edit.blade.php
@@ -1,22 +1,26 @@
 @props(['mode'=>'edit', 'taxonInfo'=>null])
-<div id="taxonomy-synonym-container" name="taxonomy-synonym-container"
-    x-data="{isValid: false,
-    acceptedstr: '',
-    tidaccepted: '',
-    init() {
-        this.validateAcceptedStatusForm();
-    },
-    async validateAcceptedStatusForm() {
-        this.acceptedstr = this.$el.querySelector('#acceptedstr')?.value ?? '';
-        this.tidaccepted = this.$el.querySelector('[name=tidaccepted]')?.value ?? '';
-        console.log('Validating form with acceptedstr:', this.acceptedstr, 'and tidaccepted:', this.tidaccepted);
-        if (this.acceptedstr && this.tidaccepted) {
-            this.isValid = true;
-        } else {
-            this.isValid = false;
-        }
-    }}"
-    >
+<div
+    id="taxonomy-synonym-container"
+    name="taxonomy-synonym-container"
+    x-data="{
+        isValid: false,
+        acceptedstr: '',
+        tidaccepted: '',
+        init() {
+            this.validateAcceptedStatusForm();
+        },
+        async validateAcceptedStatusForm() {
+            this.acceptedstr = this.$el.querySelector('#acceptedstr')?.value ?? '';
+            this.tidaccepted = this.$el.querySelector('[name=tidaccepted]')?.value ?? '';
+            console.log('Validating form with acceptedstr:', this.acceptedstr, 'and tidaccepted:', this.tidaccepted);
+            if (this.acceptedstr && this.tidaccepted) {
+                this.isValid = true;
+            } else {
+                this.isValid = false;
+            }
+        },
+    }"
+>
     <form
         id="taxonomic-status-edit-form"
         method="POST"
@@ -29,7 +33,7 @@
         @if(count($taxonInfo->synonyms ?? []) < 1)
             <span>{{ __('taxonomy_taxoneditor.NO_SYN_LINKED_TAXON') }}</span>
         @else
-            @foreach ($taxonInfo->synonyms as $synonym)
+            @foreach($taxonInfo->synonyms as $synonym)
                 <div class="mb-2 flex items-center gap-2 rounded border p-2">
                     <span>{{ $synonym->sciName ?? 'Name missing' }}</span>
                 </div>
@@ -48,10 +52,18 @@
                 hide_selector="true"
                 hide_synonyms_checkbox="true"
             />
-            <x-input name="unacceptabilityreason" id="unacceptabilityreason" label="{{ __('taxonomy_taxoneditor.REASON') }}" />
+            <x-input
+                name="unacceptabilityreason"
+                id="unacceptabilityreason"
+                label="{{ __('taxonomy_taxoneditor.REASON') }}"
+            />
             <x-input name="notes" id="notes" label="{{ __('projects.NOTES') }}" />
-            <x-button x-bind:disabled="!isValid" type="submit" class="mt-4"
-            x-text=" isValid ? '{{ $taxonInfo->isAccepted ? __('taxonomy_taxoneditor.CHANGE_STAT_NOT_ACCEPT') : __('taxonomy_taxoneditor.CHANGE_STAT_ACCEPT') }}' : '{{ __('taxonomy_taxonomyloader.SUBMISSION_DISABLED') }}'">
+            <x-button
+                x-bind:disabled="!isValid"
+                type="submit"
+                class="mt-4"
+                x-text=" isValid ? '{{ $taxonInfo->isAccepted ? __('taxonomy_taxoneditor.CHANGE_STAT_NOT_ACCEPT') : __('taxonomy_taxoneditor.CHANGE_STAT_ACCEPT') }}' : '{{ __('taxonomy_taxonomyloader.SUBMISSION_DISABLED') }}'"
+            >
             </x-button>
             <span>*{{ __('taxonomy_taxoneditor.SYNONYMS_TRANSFERRED') }}</span>
         </x-fieldset>
