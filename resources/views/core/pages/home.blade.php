a23b90456c1a570ccd70f8fa102703f77b35ceea
MuchQuak <loganpwilt@gmail.com>
2026-05-14 14:44:32 -0700

    Cleans up areas where the end tags don't match
    # Summary
    Cleanup for something I have noticed scattered around the various pages.

--- resources/views/core/pages/home.blade.php
+++ resources/views/core/pages/home.blade.php
@@ -4,4 +4,4 @@
         <h1 class="text-primary my-3 font-sans text-3xl font-bold">{!! __('home.welcome-title') !!}</h1>
         <p class="font-sans text-base">{!! __('home.welcome-text') !!}</p>
     </div>
-    </x-layout>
+</x-margin-layout>
