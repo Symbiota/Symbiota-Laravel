27c72e7d5206a9928ca1d9a0a03285524918069e
Logan Wilt <91149982+MuchQuak@users.noreply.github.com>
2026-05-15 16:15:13 -0700

    Layout optimization (#160)
    * Simply layout and use main for appbody for accessiblity
    
    * Add notes to components page
    
    * Fix typos

--- resources/js/htmx.js
+++ resources/js/htmx.js
@@ -2,3 +2,27 @@ import htmx from "htmx.org";
 import "./htmx-extensions/html-streaming";
 
 window.htmx = htmx;
+
+/* This Script cleans up alpine dom manipulations before htmx snapshots the page.
+ * Note Other solutions that were tried
+ *
+ * 1. Snapshotting before alpine does the dom manipulations.
+ *  - Sort of worked, but htmx history failed
+ */
+document.addEventListener("htmx:beforeHistorySave", (evt) => {
+    document.querySelectorAll("[x-for]").forEach((item) => {
+        item._x_lookup &&
+            Object.values(item._x_lookup).forEach((el) => el.remove());
+    });
+    document.querySelectorAll("[x-if]").forEach((item) => {
+        item._x_currentIfEl && item._x_currentIfEl.remove();
+    });
+
+    document.querySelectorAll("[x-teleport]").forEach((item) => {
+        item._x_teleport && item._x_teleport.remove();
+    });
+
+    if (window.tinymce_editor) {
+        window.tinymce_editor.remove();
+    }
+});
