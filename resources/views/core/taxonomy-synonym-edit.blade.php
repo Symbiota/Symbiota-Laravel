2c01bcee6927d66c1a70e5f4e2fc1edbfde07de5
atticus29 <mark.aaron.fisher@gmail.com>
2026-05-21 12:56:43 -0700

    move the form branching logic to make more clean

--- resources/views/core/taxonomy-synonym-edit.blade.php
+++ resources/views/core/taxonomy-synonym-edit.blade.php
@@ -28,58 +28,48 @@
         },
     }"
 >
-    <form
-        id="taxonomic-status-edit-form"
-        method="POST"
-        action="{{ route('taxon.changeAccepted') }}"
-        @change="await validateAcceptedStatusForm()"
-    >
-        @csrf
-        <x-input type="hidden" name="mode" id="mode" :value="$mode" />
-        <x-input type="hidden" name="edit-type" id="edit-type" value="synonymedits" />
-        <x-input type="hidden" name="tid" id="tid" :value="$taxonInfo->tid ?? ''" />
+    <x-fieldset>
+        <legend class="text-lg font-bold">
+            {{ $taxonInfo->sciname ?? __('taxonomy_taxoneditor.ACCEPTANCE_STATUS') }}
+        </legend>
+        @if($taxonInfo->isAccepted ?? false)
+            <span class="text-success-darker">{{ __('taxonomy_taxoneditor.ACCEPTED') }}</span>
+        @else
+            <span class="text-error-darker">{{ __('taxonomy_taxoneditor.SYNONYM') }}</span>
+        @endif
+    </x-fieldset>
+    @if(count($taxonInfo->acceptedArr ?? []) > 0)
         <x-fieldset>
-            <legend class="text-lg font-bold">{{ $taxonInfo->sciname ?? __('taxonomy_taxoneditor.ACCEPTANCE_STATUS') }}</legend>
-            @if ($taxonInfo->isAccepted ?? false)
-                <span class="text-success-darker">{{ __('taxonomy_taxoneditor.ACCEPTED')}}</span>
-            @else
-                <span class="text-error-darker">{{ __('taxonomy_taxoneditor.SYNONYM')}}</span>    
-            @endif
-        </x-fieldset>
-        @if(count($taxonInfo->acceptedArr ?? []) > 0)
-            <x-fieldset>
-                <legend class="text-lg font-bold">{{ __('taxonomy_taxoneditor.ACCEPTED_TAXON') }}</legend>
-                <ul>
-                @foreach ($taxonInfo->acceptedArr ?? [] as $tidAccepted => $linkedTaxonArr)
+            <legend class="text-lg font-bold">{{ __('taxonomy_taxoneditor.ACCEPTED_TAXON') }}</legend>
+            <ul>
+                @foreach($taxonInfo->acceptedArr ?? [] as $tidAccepted => $linkedTaxonArr)
                     <li id="acclink-{{ $tidAccepted }}">
-                        <x-link href="{{url('/taxon/' . $tidAccepted)}}">
+                        <x-link href="{{ url('/taxon/' . $tidAccepted) }}">
                             <i>{{ $linkedTaxonArr["sciname"] }}</i>
                         </x-link>
                         {{ $linkedTaxonArr["author"] ?? '' }}
-                        @if (count($taxonInfo->acceptedArr ?? []) > 1)
+                        @if(count($taxonInfo->acceptedArr ?? []) > 1)
                             <span class="hidden">
                                 <a href="{{ url('/taxon/' . $tid) }}">
-                                {{-- @TODO add delete icon --}}
+                                    {{-- @TODO add delete icon --}}
                                 </a>
                             </span>
                         @endif
-                        @if ($linkedTaxonArr["usagenotes"])
-                            <div>
-                                <u>Notes</u>: {{ $linkedTaxonArr["usagenotes"] }}
-                            </div>
+                        @if($linkedTaxonArr["usagenotes"])
+                            <div><u>Notes</u>: {{ $linkedTaxonArr["usagenotes"] }}</div>
                         @endif
                     </li>
                 @endforeach
-                </ul>
-            </x-fieldset>
-        @endif
-        @if(count($taxonInfo->synonyms ?? []) > 0)
-            <x-fieldset>
-                <legend class="text-lg font-bold">{{ __('taxonomy_taxoneditor.SYNONYMS') }}</legend>
-                <ul>
+            </ul>
+        </x-fieldset>
+    @endif
+    @if(count($taxonInfo->synonyms ?? []) > 0)
+        <x-fieldset>
+            <legend class="text-lg font-bold">{{ __('taxonomy_taxoneditor.SYNONYMS') }}</legend>
+            <ul>
                 @foreach($taxonInfo->synonyms as $tid => $synonym)
                     <li>
-                        <div class="flex items-center mb-2">
+                        <div class="mb-2 flex items-center">
                             <x-link href="{{ url('/taxon/' . $tid) }}">
                                 <span>{{ $synonym['sciname'] ?? __('taxonomy_taxoneditor.NAME_MISSING') }}</span>
                             </x-link>
@@ -87,16 +77,27 @@
                                 type="button"
                                 class="ml-2 text-sm"
                                 @click="
-                                console.log('Clicked here. @TODO implement a small editor form');
-                                "
-                            >{{ __('taxonomy_taxoneditor.EDIT_SYNONYM_LINKS') }}</x-button>
+                            console.log('Clicked here. @TODO implement a small editor form');
+                            "
+                                >{{ __('taxonomy_taxoneditor.EDIT_SYNONYM_LINKS') }}</x-button
+                            >
                         </div>
                     </li>
                 @endforeach
-                </ul>
-            </x-fieldset>
-        @endif
-        @if($taxonInfo->isAccepted ?? false)
+            </ul>
+        </x-fieldset>
+    @endif
+    @if($taxonInfo->isAccepted ?? false)
+        <form
+            id="taxonomic-status-edit-form"
+            method="POST"
+            action="{{ route('taxon.changeAccepted') }}"
+            @change="await validateAcceptedStatusForm()"
+        >
+            @csrf
+            <x-input type="hidden" name="mode" id="mode" :value="$mode" />
+            <x-input type="hidden" name="edit-type" id="edit-type" value="synonymedits" />
+            <x-input type="hidden" name="tid" id="tid" :value="$taxonInfo->tid ?? ''" />
             <x-fieldset>
                 <legend class="text-lg font-bold">{{ __('taxonomy_taxoneditor.CONVERT_TO_SYNONYM') }}</legend>
                 <x-taxa-search
@@ -119,39 +120,41 @@
                     x-bind:disabled="!isValid"
                     type="submit"
                     class="mt-4"
-                    x-text=" isValid ? '{{ __('taxonomy_taxoneditor.CHANGE_STAT_NOT_ACCEPT')}}' : '{{ __('taxonomy_taxonomyloader.SUBMISSION_DISABLED') }}'"
+                    x-text=" isValid ? '{{ __('taxonomy_taxoneditor.CHANGE_STAT_NOT_ACCEPT') }}' : '{{ __('taxonomy_taxonomyloader.SUBMISSION_DISABLED') }}'"
                 >
                 </x-button>
-                <span x-show="!isValid" class="text-red-500" id="error-container" name="error-container" x-text="errorMessage"></span>
+                <span
+                    x-show="!isValid"
+                    class="text-red-500"
+                    id="error-container"
+                    name="error-container"
+                    x-text="errorMessage"
+                ></span>
                 <span>*{{ __('taxonomy_taxoneditor.SYNONYMS_TRANSFERRED') }}</span>
             </x-fieldset>
         </form>
-        @else
-            </form>
-            <form method="POST" action="{{ route('taxon.changeToNotAccepted') }}">
-                @csrf
-                <x-fieldset>
-                    <legend class="text-lg font-bold">{{ __('taxonomy_taxoneditor.CHANGE_TO_ACCEPTED') }}</legend>
-                    <x-radio
-                        name="switchacceptance"
-                        id="switchacceptance"
-                        label="{{ __('taxonomy_taxoneditor.SWITCH_ACCEPTANCE') }}"
-                        :options="[['value' => '1', 'label' => __('taxonomy_taxoneditor.YES')], ['value' => '0', 'label' => __('taxonomy_taxoneditor.NO')]]"
-                        required
-                    />
-                    <x-button
-                        type="submit"
-                        class="mt-4"
-                        x-text="'{{ __('taxonomy_taxoneditor.CHANGE_STATUS_ACCEPTED') }}'"
-                    >
-                    </x-button>
-                    <x-input type="hidden" name="tid" id="tid" :value="$taxonInfo->tid ?? ''" />
-                    @php
-                        $firstKey = array_keys($taxonInfo->acceptedArr)[0]; // @TODO fix this
-                    @endphp
-                    <x-input type="hidden" name="new-tid" id="new-tid" :value="$firstKey ?? ''" />
-                </x-fieldset>
-            </form>
-        @endif   
-    </form>
+    @else
+        <form method="POST" action="{{ route('taxon.changeToNotAccepted') }}">
+            @csrf
+            <x-input type="hidden" name="mode" id="mode" :value="$mode" />
+            <x-input type="hidden" name="edit-type" id="edit-type" value="synonymedits" />
+            <x-input type="hidden" name="tid" id="tid" :value="$taxonInfo->tid ?? ''" />
+            <x-fieldset>
+                <legend class="text-lg font-bold">{{ __('taxonomy_taxoneditor.CHANGE_TO_ACCEPTED') }}</legend>
+                <x-radio
+                    name="switchacceptance"
+                    id="switchacceptance"
+                    label="{{ __('taxonomy_taxoneditor.SWITCH_ACCEPTANCE') }}"
+                    :options="[['value' => '1', 'label' => __('taxonomy_taxoneditor.YES')], ['value' => '0', 'label' => __('taxonomy_taxoneditor.NO')]]"
+                    required
+                />
+                <x-button type="submit" class="mt-4" x-text="'{{ __('taxonomy_taxoneditor.CHANGE_STATUS_ACCEPTED') }}'">
+                </x-button>
+                @php
+                    $firstKey = array_key_first($taxonInfo->acceptedArr ?? []);
+                @endphp
+                <x-input type="hidden" name="new-tid" id="new-tid" :value="$firstKey ?? ''" />
+            </x-fieldset>
+        </form>
+    @endif
 </div>
