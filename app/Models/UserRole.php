179e219e1cc6f4883349f649915fa3d57f44141a
MuchQuak <loganpwilt@gmail.com>
2026-05-26 16:15:37 -0700

    Add user roles roles function to get all roles

--- app/Models/UserRole.php
+++ app/Models/UserRole.php
@@ -53,11 +53,32 @@ class UserRole extends Model {
 
     public const GLOSSARY_EDITOR = 'GlossaryEditor';
 
-    public const PERSONAL_OBS_ADMIN = 'PersonalObsAdmin';
+    public const PERSONAL_OBS_ADMIN = 'PersonalObsAdmin'; // Todo is in current code base but not assignable?
 
-    public const PERSONAL_OBS_EDITOR = 'PersonalObsEditor';
+    public const PERSONAL_OBS_EDITOR = 'PersonalObsEditor'; // Todo is in current code base but not assignable?
 
-    public const PERSONAL_OBS_READER = 'PersonalObsReader';
+    public const PERSONAL_OBS_READER = 'PersonalObsReader'; // Todo is in current code base but not assignable?
+
+    public static function roles(): array {
+        return [
+            self::SUPER_ADMIN,
+            self::RARE_SPP_ADMIN,
+            self::RARE_SPP_READER_ALL,
+            self::RARE_SPP_READER,
+            self::DATASET_ADMIN,
+            self::DATASET_EDITOR,
+            self::TAXONOMY,
+            self::TAXON_PROFILE,
+            self::KEY_ADMIN,
+            self::KEY_EDITOR,
+            self::COLL_ADMIN,
+            self::COLL_EDITOR,
+            self::PROJ_ADMIN,
+            self::CL_ADMIN,
+            self::CL_CREATE,
+            self::GLOSSARY_EDITOR,
+        ];
+    }
 
     public function user() {
         return $this->belongsTo(User::class, 'uid', 'uid');
