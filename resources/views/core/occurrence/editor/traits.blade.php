c4513bdaefed00ffb3605fc017258fefbbbfe09a
MuchQuak <loganpwilt@gmail.com>
2026-06-08 09:53:31 -0700

    more traits progress

--- resources/views/core/occurrence/editor/traits.blade.php
+++ resources/views/core/occurrence/editor/traits.blade.php
@@ -1,52 +1,43 @@
 @props(['occurrence', 'traits' => []])
 <div class="flex flex-col gap-4">
-    <div class="flex flex-col gap-4 border p-4">
-        @foreach($traits as $traitID => $traitData)
-        @php if(isset($traitData['dependentTrait'])) continue @endphp
+    @foreach($traits as $traitID => $traitData)
+    @php if(isset($traitData['dependentTrait'])) continue @endphp
+    <form>
         <x-fieldset :legend="$traitData['name']">
             {{-- Numerical --}}
             <x-traits.form-input :traits="$traits" :traitId="$traitID" />
-        </x-fieldset>
-        @endforeach
-        <div>
-            <div class="flex">
-                <h3 class="text-lg font-bold">Anglosperm Phenolgical Traits</h3>
+            <x-input :inline="true" label="Notes" />
+            <x-select
+                class="w-60"
+                label="Source"
+                :inline="true"
+                :items="[
+                ['title' => 'Machine Learning', 'value' => 'machine_learning', 'disabled' => false],
+                ['title' => 'Physical Specimen', 'value' => 'physical_specimen', 'disabled' => false],
+                ['title' => 'Viewing Image', 'value' => 'viewing_image', 'disabled' => false],
+                ['title' => 'Verbatim Text Mining', 'value' => 'verbatim_text_mining', 'disabled' => false],
+            ]"
+            />
+
+            <x-select
+                class="w-60"
+                label="Status"
+                :inline="true"
+                :items="[
+                    item(0, 'Not Reviewed'),
+                    item(5, 'Expert Needed'),
+                    item(10, 'Reviewed'),
+            ]"
+            />
+            <div class="flex gap-2">
+                <x-button>
+                    {{ __('geothesaurus.SAVE_EDITS') }}
+                </x-button>
+                <x-button class="ml-auto" variant="error">
+                    {{ __('includes_traittab.DEL_CODING') }}
+                </x-button>
             </div>
-        </div>
-
-        {{-- TODO (Logan) Update this to have nested attribute tree--}}
-        <x-radio
-            name="pheno-traits"
-            :options="[
-            ['label' => 'Reproductive', 'value' => 'reproductive'],
-            ['label' => 'Sterile', 'value' => 'sterile'],
-            ['label' => 'Not Scorable', 'value' => 'not-scorable']
-        ]"
-        />
-
-        <div class="font-bold">Add New Resource</div>
-        <x-input label="Notes" />
-        <x-select
-            class="w-60"
-            label="Source"
-            :items="[
-            ['title' => 'Machine Learning', 'value' => 'machine_learning', 'disabled' => false],
-            ['title' => 'Physical Specimen', 'value' => 'physical_specimen', 'disabled' => false],
-            ['title' => 'Viewing Image', 'value' => 'viewing_image', 'disabled' => false],
-            ['title' => 'Verbatim Text Mining', 'value' => 'verbatim_text_mining', 'disabled' => false],
-        ]"
-        />
-
-        <x-select
-            class="w-60"
-            label="Status"
-            :items="[
-            ['title' => 'Not Reviewed', 'value' => 'not_reviewed', 'disabled' => false],
-            ['title' => 'Expert Needed', 'value' => 'expert_needed', 'disabled' => false],
-            ['title' => 'Reviewed', 'value' => 'reviewed', 'disabled' => false],
-        ]"
-        />
-        <x-button>Save Edits</x-button>
-        <x-button variant="error">Delete Coding</x-button>
-    </div>
+        </x-fieldset>
+    </form>
+    @endforeach
 </div>
