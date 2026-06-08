7d602c06acf17de87537bf66993e50ed7492e23a
MuchQuak <loganpwilt@gmail.com>
2026-06-08 15:49:01 -0700

    add slot to input field to allow for more customization

--- resources/views/core/input.blade.php
+++ resources/views/core/input.blade.php
@@ -36,4 +36,8 @@
     @elseif(!empty($assistive_text))
         <span class="assistive-text">{{ $assistive_text }}</span>
     @endif
+
+    @if($slot->isNotEmpty())
+        {{ $slot }}
+    @endif
 </div>
