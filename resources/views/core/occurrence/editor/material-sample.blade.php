6bcecc4eee0083c637f94c4042c5834cf3b225c1
MuchQuak <loganpwilt@gmail.com>
2026-06-09 11:22:12 -0700

    format map sample

--- resources/views/core/occurrence/editor/material-sample.blade.php
+++ resources/views/core/occurrence/editor/material-sample.blade.php
@@ -1,35 +1,35 @@
 @props(['material_samples' => []])
 <x-fieldset :legend="__('includes_materialsampleinclude.ADD_SAMPLE')">
-   <div class="flex items-center gap-2">
+    <div class="flex items-center gap-2">
         <x-input :label="__('material_sample.SAMPLE_TYPE')" />
         <x-input :label="__('collections_list.CATALOG_NUMBER')" />
         <x-input :label="__('material_sample.MATERIAL_SAMPLE_GUID')" />
-   </div>
+    </div>
 
-   <div class="flex items-center gap-2">
+    <div class="flex items-center gap-2">
         <x-input :label="__('material_sample.SAMPLE_CONDITION')" />
         <x-input :label="__('individual.DISPOSITION')" />
         <x-input :label="__('material_sample.PRESERVATION_TYPE')" />
-   </div>
+    </div>
 
-   <div class="flex items-center gap-2">
+    <div class="flex items-center gap-2">
         <x-input type="date" :label="__('material_sample.PREPARATION_DATE')" />
         <x-input :label="__('material_sample.PREPARED_BY')" />
-   </div>
+    </div>
 
     <x-input :label="__('material_sample.PREPARATION_DETAILS')" />
 
-   <div class="flex items-center gap-2">
+    <div class="flex items-center gap-2">
         <x-input :label="__('individual.INDIVIDUAL_COUNT')" />
         <x-input :label="__('material_sample.SAMPLE_SIZE')" />
         <x-input :label="__('individual.STORAGE_LOC')" />
-   </div>
+    </div>
 
     <x-input :label="__('georef_batchgeoreftool.REMARKS')" />
 
-   <x-button>{{ __('includes_materialsampleinclude.ADD_SAMPLE') }}</x-button>
+    <x-button>{{ __('includes_materialsampleinclude.ADD_SAMPLE') }}</x-button>
 
-   @foreach($material_samples as $sample)
+    @foreach($material_samples as $sample)
 
-   @endforeach
+    @endforeach
 </x-fieldset>
