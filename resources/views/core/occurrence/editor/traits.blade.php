a6797f74f5908d4006f0da3305ce438c177935e8
MuchQuak <loganpwilt@gmail.com>
2026-06-09 11:21:45 -0700

    move traits data pull into trait tab view

--- resources/views/core/occurrence/editor/traits.blade.php
+++ resources/views/core/occurrence/editor/traits.blade.php
@@ -1,32 +1,63 @@
-@props(['occurrence', 'traits' => []])
+@props(['occurrence' ])
+
+@php global $SERVER_ROOT;
+
+include_once(legacy_path('/classes/OccurrenceAttributes.php'));
+$attrManager = new OccurrenceAttributes();
+$attrManager->setOccid($occurrence->occid);
+
+$traits = $attrManager->getTraitArr();
+$source_items = [];
+
+if($traits) {
+    foreach($traits as $traitID => $traitData){
+        if(!isset($traitData['dependentTrait'])){
+            $statusCode = 0;
+            $notes = '';
+            $source = '';
+            if(array_key_exists('states', $traitData)){
+                foreach($traitData['states'] as $id => $stArr){
+                    if(isset($stArr['statuscode']) && $stArr['statuscode']) $statusCode = $stArr['statuscode'];
+                    if(isset($stArr['notes']) && $stArr['notes']) $notes = $stArr['notes'];
+                    if(isset($stArr['source']) && $stArr['source']) $source = $stArr['source'];
+                }
+            }
+        }
+    }
+
+    foreach($attrManager->getSourceControlledArr($source) as $controlledSource) {
+        $source_items[] = item($controlledSource, $controlledSource);
+    }
+}
+@endphp
+
 <div class="flex flex-col gap-4">
     @foreach($traits as $traitID => $traitData)
     @php if(isset($traitData['dependentTrait'])) continue @endphp
-    <form>
+    <form method="POST">
+        @csrf
         <x-fieldset :legend="$traitData['name']">
             {{-- Numerical --}}
             <x-traits.form-input :traits="$traits" :traitId="$traitID" />
-            <x-input :inline="true" label="Notes" />
+            <x-input name="notes" :inline="true" label="Notes" />
             <x-select
                 class="w-60"
-                label="Source"
+                name="source"
+                :label="__('exsiccati.SOURCE')"
                 :inline="true"
-                :items="[
-                ['title' => 'Machine Learning', 'value' => 'machine_learning', 'disabled' => false],
-                ['title' => 'Physical Specimen', 'value' => 'physical_specimen', 'disabled' => false],
-                ['title' => 'Viewing Image', 'value' => 'viewing_image', 'disabled' => false],
-                ['title' => 'Verbatim Text Mining', 'value' => 'verbatim_text_mining', 'disabled' => false],
-            ]"
+                :items="$source_items"
             />
 
             <x-select
                 class="w-60"
-                label="Status"
+                name="setstatus"
+                :label="__('taxonomy_batchloader.STATUS')"
+                default="0"
                 :inline="true"
                 :items="[
-                    item(0, 'Not Reviewed'),
-                    item(5, 'Expert Needed'),
-                    item(10, 'Reviewed'),
+                    item(0, __('includes_traittab.NOT_REVIEWED')),
+                    item(5, __('includes_traittab.EXPERT_NEEDED')),
+                    item(10, __('misc_commentlist.REVIEWED')),
             ]"
             />
             <div class="flex gap-2">
