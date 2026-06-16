ca60d9b69854b4f34c9fe2104820762f26ab5442
atticus29 <mark.aaron.fisher@gmail.com>
2026-06-15 18:43:50 -0700

    format

--- resources/views/core/child-list.blade.php
+++ resources/views/core/child-list.blade.php
@@ -2,21 +2,21 @@
 @if(count($children) < 1)
     <p>{{ __('taxonomy_taxonomyloader.NO_CHILD_TAXA') }}</p>
 @else
-<ul>
-    @foreach($children as $childTid =>$child)
-        <li>
-            <div>
-                <x-link href="{{ url('taxon/' . $childTid) }}">
-                    <i>{{ $child['sciname'] ?? '' }}</i> {{ $child['author'] ?? '' }}
-                </x-link>
-                @if(!empty($child['accTid']) && intval($child['accTid']) !== $childTid)
-                    <span>&#10140</span>
-                    <x-link href="{{ url('taxon/' . $child['accTid']) }}">
-                        <i>{{ $child['accSciname'] ?? $child['sciname'] ?? '' }}</i> {{ $child['accAuthor'] ?? '' }}
+    <ul>
+        @foreach($children as $childTid =>$child)
+            <li>
+                <div>
+                    <x-link href="{{ url('taxon/' . $childTid) }}">
+                        <i>{{ $child['sciname'] ?? '' }}</i> {{ $child['author'] ?? '' }}
                     </x-link>
-                @endif
-            </div>
-        </li>
-    @endforeach
-</ul>
+                    @if(!empty($child['accTid']) && intval($child['accTid']) !== $childTid)
+                        <span>&#10140</span>
+                        <x-link href="{{ url('taxon/' . $child['accTid']) }}">
+                            <i>{{ $child['accSciname'] ?? $child['sciname'] ?? '' }}</i> {{ $child['accAuthor'] ?? '' }}
+                        </x-link>
+                    @endif
+                </div>
+            </li>
+        @endforeach
+    </ul>
 @endif
