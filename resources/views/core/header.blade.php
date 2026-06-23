9b548d520fc61022c5d99a2a9055d5102077702f
MuchQuak <loganpwilt@gmail.com>
2026-06-23 10:40:45 -0700

    format

--- resources/views/core/header.blade.php
+++ resources/views/core/header.blade.php
@@ -4,18 +4,20 @@
     class="text-banner-overlay-content min-h-28 bg-cover bg-position-[var(--symb-banner-position)]"
     style="background-image: var(--symb-banner-url)"
 >
-    <div {{ $attributes->twMerge('bg-banner-overlay flex items-center gap-4 min-h-28 w-full flex-wrap py-4 px-4 md:px-8') }}>
+    <div
+        {{ $attributes->twMerge('bg-banner-overlay flex items-center gap-4 min-h-28 w-full flex-wrap py-4 px-4 md:px-8') }}
+    >
         <div class="flex flex-1 items-center gap-4">
             @if(config('portal.show_brand'))
-                <div class="h-fit lg:w-[7.5rem] w-[4.5rem]">
+                <div class="h-fit w-[4.5rem] lg:w-[7.5rem]">
                     <a href="https://symbiota.org">
                         <x-brand />
                     </a>
                 </div>
             @endif
             <div class="flex flex-col justify-center font-bold text-shadow-lg">
-                <h1 class="text-xl md:text-2xl lg:text-4xl text-nowrap">{{ config('portal.header_title') }}</h1>
-                <h2 class="text-xs md:text-sm lg:text-base text-nowrap">{{ config('portal.header_sub_title') }}</h2>
+                <h1 class="text-xl text-nowrap md:text-2xl lg:text-4xl">{{ config('portal.header_title') }}</h1>
+                <h2 class="text-xs text-nowrap md:text-sm lg:text-base">{{ config('portal.header_sub_title') }}</h2>
             </div>
         </div>
 
